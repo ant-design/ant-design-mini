9bcef896d7bde087cebdd70c71a6a36cfd07c9da
shadyzoz <shadyzoz@163.com>
2022-02-15 18:00:34 +0800

    chore(*): cleanup

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  displayName: 'antd-mini',
-  preset: '@ali/minist/jest-preset/ts',
-  rootDir: './',
-  testMatch: ['<rootDir>/tests/pages/**/__tests__/**/*.spec.ts'],
-  collectCoverageFrom: ['<rootDir>/tests/es/**/*.{ts,js}'],
-};
