3642932f81a95bb424d5cc63197f82bbcdc988c6
彧衡 <yuheng.lh@antgroup.com>
2022-03-31 18:41:45 +0800

    fix(image-upload): add preview default value for image-upload

--- src/ImageUpload/props.js
+++ src/ImageUpload/props.js
@@ -1,11 +1,12 @@
 export const UploaderDefaultProps = {
   defaultValue: [],
   enableShowPhotoDownload: false,
-  maxCount: 1,
-  mode: 'scaleToFill',
   filename: 'image',
   formData: {},
   height: 80,
+  maxCount: 1,
+  mode: 'scaleToFill',
+  preview: true,
   width: 80,
   value: [],
 };
