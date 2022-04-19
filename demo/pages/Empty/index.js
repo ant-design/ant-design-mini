69b0218859f523b70f1f0bb8f29add957dae7b9f
彧衡 <yuheng.lh@antgroup.com>
2022-04-20 01:32:28 +0800

    feat(empty): add empty compoennt

---
+++ demo/pages/Empty/index.js
@@ -0,0 +1,28 @@
+Page({
+  data: {
+    mode: 'page',
+    modeList: [{
+      name: 'page',
+      value: '全页空状态',
+      checked: true
+    }, {
+      name: 'area',
+      value: '局部空状态',
+    }]
+  },
+  onSubBtnClick() {
+    my.alert({
+      title: '你点击了次按钮',
+    });
+  },
+  onMainBtnClick() {
+    my.alert({
+      title: '你点击了主按钮',
+    });
+  },
+  handleChangeMode(e) {
+    this.setData({
+      mode: e.detail.value
+    })
+  }
+});
---
+++ src/Empty/props.js
@@ -0,0 +1,8 @@
+export const EmptyDefaultProps = {
+  mainButtonText: '操作2',
+  mainText: '这里什么都没有',
+  mode: 'page',
+  subButtonText: '操作1',
+  subText: '看看其它的吧'
+};
+
