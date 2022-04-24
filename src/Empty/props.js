61b634ab40730a70397fea4d0a4462041444e9ff
彧衡 <yuheng.lh@antgroup.com>
2022-04-24 17:40:23 +0800

    feat(empty): 删除多余属性

--- src/Empty/props.js
+++ src/Empty/props.js
@@ -2,8 +2,6 @@ export const EmptyDefaultProps = {
   mainButtonText: '操作2',
   mainText: '这里什么都没有',
   mode: 'page',
-  showMainText: true,
-  showSubText: true,
   subButtonText: '操作1',
   subText: '看看其它的吧'
 };
