ad49de349f600389c20669df458c6f3a952c5d6e
彧衡 <yuheng.lh@antgroup.com>
2022-05-19 01:29:35 +0800

    feat(tree-select): new component

---
+++ src/TreeSelect/props.d.ts
@@ -0,0 +1,54 @@
+import { IBaseProps } from '../_base';
+
+/**
+ * @description 级联选择
+ */
+
+export type TreeSelectValue = string;
+
+export type TreeSelectOption = {
+  /**
+   * @description 当前选项名称
+   */
+  label: string;
+
+  /**
+   * @description 当前选项值
+   */
+  value: string;
+
+  /**
+   * @description 当前选项是否禁用
+   */
+  disabled?: boolean;
+
+  /**
+   * @description 当前选项的下一级列表
+   */
+  children?: TreeSelectOption[];
+};
+
+export interface ITreeSelectProps extends IBaseProps {
+  /**
+   * @description 树形选择的数据源，目前只支持两级或三级树形选择
+   * @default []
+   */
+  options: TreeSelectOption[];
+
+  /**
+   * @description 当前选项的值
+   */
+  value?: TreeSelectValue[];
+
+  /**
+   * @description 当前组件是否以蒙层的形式出现
+   * @default false
+   */
+  visible?: boolean;
+
+  /**
+   * @description 当组件的值变化时触发
+   */
+  onChange?: (value: TreeSelectValue[]) => void;
+}
+export declare const treeSelectDefaultProps: Partial<ITreeSelectProps>;
