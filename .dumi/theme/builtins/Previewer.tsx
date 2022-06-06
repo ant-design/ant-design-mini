9515df927a0154ccd45b478237f4961670e2724a
wyj <wb-wyj631405@antfin.com>
2022-06-06 09:39:29 +0800

    feat(herbox): add herbox iframe loading

--- .dumi/theme/builtins/Previewer.tsx
+++ .dumi/theme/builtins/Previewer.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Spin from 'antd/lib/spin';
+import 'antd/lib/spin/style/index';
 import './Previewer.less';
 
 interface IProps {
   herboxUrl: string;
 }
 const Previewer: React.FC<IProps> = (props) => {
+  const [loaded, setLoaded] = useState(false);
   return (
     <div className="previewer">
-      <iframe src={props.herboxUrl} />
+      {!loaded && <Spin spinning   className="previewer-loading" />}
+      <iframe src={props.herboxUrl} onLoad={() => setLoaded(true)} />
     </div>
   );
 };
