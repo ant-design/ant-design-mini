1e4e11f7851c8add51b39cf76cba8b10d8c1aa93
xinshu.sxs <xinshu.sxs@antgroup.com>
2022-06-06 10:06:03 +0800

    feat(ProgressBar): new Component

---
+++ src/ProgressBar/props.js
@@ -0,0 +1,9 @@
+export const ProgressBarDefaultProps = {
+  progress: 0,
+  height: 16,
+  width: 606,
+  showDefaultIndicator: true,
+  className: '',
+  orbitColor: "#e5e5e5",
+  progressColor: "#1677FF",
+};
