9bae789933fe1e6e85fc8613171df4ce9131d968
彧衡 <yuheng.lh@antgroup.com>
2022-06-24 11:30:07 +0800

    feat(divider): new component

--- demo/utils/constants.js
+++ demo/utils/constants.js
@@ -71,6 +71,11 @@ export const componentList = [
         nameZN: '滑动面板',
         path: '/pages/FloatPanel/index',
       },
+      {
+        name: 'Divider',
+        nameZN: '分割线',
+        path: '/pages/Divider/index',
+      },
     ],
   },
   {
