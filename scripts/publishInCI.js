8da7e643e3f892a547ba9a41932af1fd8349782c
野声 <lijiacheng.ljc@antgroup.com>
2022-06-30 12:25:39 +0800

    ci: update release

--- scripts/publishInCI.js
+++ scripts/publishInCI.js
@@ -4,26 +4,23 @@ const {
   generateSematicVersion,
 } = require('./publishUtils');
 
-console.log('dist tag:', process.env.DIST_TAG);
+let distTag = process.env.DIST_TAG;
+
+console.log('dist tag:', distTag);
 
 let version = process.env.DIST_VERSION;
-let versionLevel = process.env.DIST_VERSION_LEVEL;
 if (!version) {
   const currentVersion = require('../package.json').version;
   console.log(`currentVersion`, currentVersion);
-  version = generateSematicVersion(
-    process.env.DIST_TAG,
-    versionLevel ?? 'patch',
-    currentVersion
-  );
+  version = generateSematicVersion(distTag, 'patch', currentVersion);
 }
 
 console.log('version:', version);
 
-doPublish(process.env.DIST_TAG, version, process.env.DRY_RUN);
+doPublish(distTag, version, Boolean(process.env.SETUP_RELEASE));
 
-if (process.env.DIST_VERSION && !process.env.DRY_RUN) {
-  // 只对 release 的分支打 Tag
+// 指定了 DIST_VERSION 和 SETUP_RELEASE，说明是执行了 release 这个 workflow，不需要真实的发布的 npm 和 release，只需要上传 tag
+if (process.env.DIST_VERSION && process.env.SETUP_RELEASE) {
   execSync(`git tag ${process.env.DIST_VERSION}`);
   execSync(
     `git push origin ${process.env.DIST_VERSION}:${process.env.DIST_VERSION}`
