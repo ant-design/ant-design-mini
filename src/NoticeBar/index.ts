4aa36b84bded5ef09559245c42498d73f328371f
wyj580231 <30888500+wyj580231@users.noreply.github.com>
2022-08-15 18:14:12 +0800

    fix(NoticeBar): fix marquee on pageHide (#225)
    * fix(NoticeBar): fix marquee on pageHide
    
    * feat(NoticeBar): remove useless code
    
    * feat(NoticeBarMarquee): remove _ in varibale name
    
    * feat(NoticeBarMarquee): change trailing to 200
    
    * feat(NoticeBarMarqueen): delete scrollLeft
    
    * feat(NoticeBarMarquee): add onShow hanlder
    
    * feat(NoticeBarMarquee): add NoticeBarMarquee type
    
    * chore(NoticeBar): delete typeof trailing
    
    Co-authored-by: wyj <wyj@wyj.com>
    Co-authored-by: 陌缓 <dongqing.mdq@alibaba-inc.com>

--- src/NoticeBar/index.ts
+++ src/NoticeBar/index.ts
@@ -1,58 +1,58 @@
-import { NoticeBarDefaultProps } from './props';
+import { NoticeBarDefaultProps, INoticeBarData, INoticeBarMethods, INoticeBarProps } from './props';
 import { log } from '../_util/console';
 import { IBoundingClientRect } from "../_base"
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const noop = () => { };
-const _canIUseTransitionEnd = my.canIUse('view.onTransitionEnd');
 
-Component({
+Component<INoticeBarData, Partial<INoticeBarProps>, INoticeBarMethods>({
   props: NoticeBarDefaultProps,
   data: {
-    _show: true,
-    _canIUseTransitionEnd,
-    _marqueeStyle: '',
-    _animatedWidth: 0,
-    _overflowWidth: 0,
-    _duration: 0,
-    _viewWidth: 0,
+    show: true,
+    marqueeStyle: '',
+    animatedWidth: 0,
+    overflowWidth: 0,
+    duration: 0,
+    viewWidth: 0,
   },
   didMount() {
     const { enableMarquee } = this.props;
     this.showError();
 
     if (enableMarquee) {
-      if (!_canIUseTransitionEnd) {
-        this._measureText();
-        this._startAnimation();
-      } else {
-        this._measureText(this.startMarquee.bind(this));
-      }
+      this.measureText(this.startMarquee.bind(this));
     }
   },
 
   didUpdate() {
     const { enableMarquee } = this.props;
     this.showError();
-
     // 这里更新处理的原因是防止notice内容在动画过程中发生改变。
     if (enableMarquee) {
-      if (!this._marqueeTimer && !_canIUseTransitionEnd) {
-        this._measureText();
-        this._startAnimation();
-      } else {
-        this._measureText(this.startMarquee.bind(this));
-      }
+      this.measureText(this.startMarquee.bind(this));
     }
   },
 
-  didUnmount() {
-    if (this._marqueeTimer) {
-      clearTimeout(this._marqueeTimer);
-      this._marqueeTimer = null;
-    }
+  pageEvents:{
+    onShow(){
+      this.resetState();
+    },
   },
+
   methods: {
+    resetState(){
+      if(this.props.enableMarquee) {
+        this.setData({
+          marqueeStyle: '',
+          animatedWidth: 0,
+          overflowWidth: 0,
+          duration: 0,
+          viewWidth: 0,
+        },()=>{
+          this.resetMarquee();
+          this.measureText(this.startMarquee.bind(this));
+        });
+      }
+     
+    },
     showError() {
       const { actions } = this.props;
       if (!Array.isArray(actions) && typeof actions !== 'undefined') {
@@ -66,7 +66,7 @@ Component({
       }
       if (mode === 'closeable' && typeof onTap === 'function') {
         this.setData({
-          _show: false,
+          show: false,
         });
         return onTap();
       }
@@ -83,42 +83,42 @@ Component({
     // 文本滚动的计算
     resetMarquee() {
       const { loop } = this.props;
-      const { _viewWidth } = this.data;
+      const { viewWidth } = this.data;
       let showMarqueeWidth = '0px';
       if (loop) {
-        showMarqueeWidth = `${_viewWidth}px`;
+        showMarqueeWidth = `${viewWidth}px`;
       }
-      const _marqueeStyle = `transform: translate3d(${showMarqueeWidth}, 0, 0); transition: 0s all linear;`;
+      const marqueeStyle = `transform: translate3d(${showMarqueeWidth}, 0, 0); transition: 0s all linear;`;
       this.setData({
-        _marqueeStyle,
+        marqueeStyle,
       });
     },
     startMarquee() {
       const { loop } = this.props;
       const leading = 500;
-      const { _duration, _overflowWidth, _viewWidth } = this.data;
-      let marqueeScrollWidth = _overflowWidth;
+      const { duration, overflowWidth, viewWidth } = this.data;
+      let marqueeScrollWidth = overflowWidth;
       if (loop) {
-        marqueeScrollWidth = _overflowWidth + _viewWidth;
+        marqueeScrollWidth = overflowWidth + viewWidth;
       }
-      const _marqueeStyle = `transform: translate3d(${-marqueeScrollWidth}px, 0, 0); transition: ${_duration}s all linear ${typeof leading === 'number' ? `${leading / 1000}s` : '0s'};`;
-      if (this.data._marqueeStyle !== _marqueeStyle) {
+      const marqueeStyle = `transform: translate3d(${-marqueeScrollWidth}px, 0, 0); transition: ${duration}s all linear ${typeof leading === 'number' ? `${leading / 1000}s` : '0s'};`;
+      if (this.data.marqueeStyle !== marqueeStyle) {
         this.setData({
-          _marqueeStyle,
+          marqueeStyle,
         });
       }
     },
     onTransitionEnd() {
       const { loop } = this.props;
-      const trailing = 800;
+      const trailing = 200;
       if (loop) {
         setTimeout(() => {
           this.resetMarquee();
-          this._measureText(this.startMarquee.bind(this));
-        }, typeof trailing === 'number' ? trailing : 0);
+          this.measureText(this.startMarquee.bind(this));
+        }, trailing);
       }
     },
-    _measureText(callback = noop) {
+    measureText(callback) {
       const fps = 40;
       const { loop } = this.props;
       // 计算文本所占据的宽度，计算需要滚动的宽度
@@ -130,69 +130,22 @@ Component({
           .boundingClientRect()
           .exec((ret) => {
             // eslint-disable-next-line max-len
-            const _overflowWidth = (ret && ret[0] && ret[1] && ((<IBoundingClientRect>ret[0]).width - (<IBoundingClientRect>ret[1]).width)) || 0;
-            const _viewWidth = (<IBoundingClientRect>ret[1])?.width || 0;
-            let marqueeScrollWidth = _overflowWidth;
+            const overflowWidth = (ret && ret[0] && ret[1] && ((<IBoundingClientRect>ret[0]).width - (<IBoundingClientRect>ret[1]).width)) || 0;
+            const viewWidth = (<IBoundingClientRect>ret[1])?.width || 0;
+            let marqueeScrollWidth = overflowWidth;
             if (loop) {
-              marqueeScrollWidth = _overflowWidth + _viewWidth;
+              marqueeScrollWidth = overflowWidth + viewWidth;
             }
-            if (_overflowWidth > 0) {
+            if (overflowWidth > 0) {
               this.setData({
-                _overflowWidth,
-                _viewWidth,
-                _duration: (marqueeScrollWidth / fps),
+                overflowWidth,
+                viewWidth,
+                duration: (marqueeScrollWidth / fps),
               });
               callback();
             }
           });
       }, 0);
-    },
-    _startAnimation() {
-      if (this._marqueeTimer) {
-        clearTimeout(this._marqueeTimer);
-      }
-
-      const loop = false;
-      const leading = 500;
-      const trailing = 800;
-      const fps = 40;
-      const TIMEOUT = 1 / fps * 1000;
-      const isLeading = this.data._animatedWidth === 0;
-      const timeout = isLeading ? leading : TIMEOUT;
-
-      const animate = () => {
-        const { _overflowWidth } = this.data;
-        let _animatedWidth = this.data._animatedWidth + 1;
-        const isRoundOver = _animatedWidth > _overflowWidth;
-
-        if (isRoundOver) {
-          if (loop) {
-            _animatedWidth = 0;
-          } else {
-            return;
-          }
-        }
-
-        if (isRoundOver && trailing) {
-          this._marqueeTimer = setTimeout(() => {
-            this.setData({
-              _animatedWidth,
-            });
-
-            this._marqueeTimer = setTimeout(animate, TIMEOUT);
-          }, trailing);
-        } else {
-          this.setData({
-            _animatedWidth,
-          });
-
-          this._marqueeTimer = setTimeout(animate, TIMEOUT);
-        }
-      };
-
-      if (this.data._overflowWidth !== 0) {
-        this._marqueeTimer = setTimeout(animate, timeout);
-      }
     },
   },
 });
