b6882e09a573207128535de5cac776d924a3fbec
Sxs7513 <526015242@qq.com>
2022-09-06 19:34:20 +0800

    feat(Rate): new Component (#155)
    * feat(Rate): new Component
    
    * feat(Rate): change css var to style
    
    * feat(Rate): support move
    
    * feat(Rate): demo ui
    
    * fix(Rate): block unexpect moveend
    
    * feat(Rate): demo
    
    * fix(Rate): touchmove 时选择器更改
    
    * feat(Rate): refine demo
    
    Co-authored-by: xinshu.sxs <xinshu.sxs@antgroup.com>
    Co-authored-by: 陌缓 <dongqing.mdq@alibaba-inc.com>

--- demo/utils/constants.js
+++ demo/utils/constants.js
@@ -191,6 +191,11 @@ export const componentList = [
         nameZN: '协议',
         path: '/pages/Terms/index',
       },
+      {
+        name: 'Rate',
+        nameZN: '星级',
+        path: '/pages/Rate/index',
+      },
     ],
   },
   {
