863c9720c7fde634567e07c0b87e097a34a6bb24
jc9702507 <jc9702507@gmail.com>
2022-11-09 10:44:11 +0800

    fix: steps examples

--- demo/pages/Steps/index.js
+++ demo/pages/Steps/index.js
@@ -19,23 +19,5 @@ Page({
       '',
       'StarFill'
     ],
-    activeIcon1: 'CheckCircleFill',
-    activeIcon2:
-      'https://gw.alipayobjects.com/mdn/rms_ce4c6f/afts/img/A*XMCgSYx3f50AAAAAAAAAAABkARQnAQ',
-  },
-  handleNextStep() {
-    this.setData({
-      activeIndex: this.data.activeIndex + 1,
-    });
-  },
-  handlePreStep() {
-    this.setData({
-      activeIndex: this.data.activeIndex - 1,
-    });
-  },
-  handleSetFailIndex() {
-    this.setData({
-      failIndex: !this.data.failIndex,
-    });
   },
 });
