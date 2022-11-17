4e117a0836e9905db0b4fd60957cc4ffcf5e6dcb
jc9702507 <jc9702507@gmail.com>
2022-11-17 09:44:50 +0800

    fix: form createComponent

--- src/Form/FormStepper/index.ts
+++ src/Form/FormStepper/index.ts
@@ -5,6 +5,12 @@ createComponent({
   props: FormStepperDefaultProps,
   initialValue: 0,
   methods: {
+    onChange(value, e) {
+      this.emit('onChange', value);
+      if (this.props.onChange) {
+        this.props.onChange(value, e);
+      }
+    },
     onBlur(e) {
       if (this.props.onBlur) {
         this.props.onBlur(e);
--- src/Form/FormSwitch/index.ts
+++ src/Form/FormSwitch/index.ts
@@ -4,4 +4,12 @@ import createComponent from '../createComponent';
 createComponent({
   props: FormSwitchDefaultProps,
   initialValue: false,
+  methods: {
+    onChange(value, e) {
+      this.emit('onChange', value);
+      if (this.props.onChange) {
+        this.props.onChange(value, e);
+      }
+    },
+  }
 });
