16b4fb4988953415de5b48ee2ca0604e0b728ee3
jc <113670357+jc9702507@users.noreply.github.com>
2022-11-22 22:34:14 +0800

    fix: loading (#365)

--- src/Loading/index.ts
+++ src/Loading/index.ts
@@ -2,30 +2,4 @@ import { LoadingDefaultProps } from './props';
 
 Component({
   props: LoadingDefaultProps,
-  data: {
-    _loading: false,
-  },
-  didMount() {
-    this.setLoading();
-  },
-  didUpdate(prevProps) {
-    if (prevProps.loading !== this.props.loading) {
-      this.setLoading();
-    }
-  },
-  methods: {
-    setLoading() {
-      const { loading } = this.props;
-
-      if (loading && !this.data._loading) {
-        this.setData({
-          _loading: true,
-        });
-      } else if (!loading && this.data._loading) {
-        this.setData({
-          _loading: false,
-        });
-      }
-    },
-  },
 });
