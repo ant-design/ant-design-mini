ed4a6e3e55373a9d2137a6ba7d59ef2dca78472d
jc9702507 <jc9702507@gmail.com>
2022-11-25 20:52:19 +0800

    fix: radio mixin value

--- src/Radio/index.ts
+++ src/Radio/index.ts
@@ -1,47 +1,22 @@
 import { RadioDefaultProps } from './props';
 import fmtEvent from '../_util/fmtEvent';
+import mixinValue from '../mixins/value';
 
 Component({
   props: RadioDefaultProps,
-  data: {
-    selfChecked: undefined,
-    hasChange: false,
-    checkedInProps: undefined,
-  },
-  onInit() {
-    this.setCheckedInProps();
-  },
-  didMount() {
-    this.setCheckedInProps();
-  },
+  mixins: [mixinValue({
+    valueKey: 'checked',
+    defaultValueKey: 'defaultChecked',
+  })],
   methods: {
-    setCheckedInProps() {
-      if (typeof this.data.checkedInProps !== 'undefined') {
-        return;
-      }
-      this.setData({
-        checkedInProps: 'checked' in this.props,
-      });
-    },
-    setChange() {
-      if (this.data.hasChange) {
-        return;
-      }
-      this.setData({
-        hasChange: true,
-      });
-    },
     onChange(e) {
       const value = e.detail.value;
-      this.setChange();
-      if (!this.data.checkedInProps) {
-        this.setData({
-          selfChecked: value,
-        });
+      if (!this.isControlled()) {
+        this.update(value);
       }
       if (this.props.onChange) {
         this.props.onChange(value, fmtEvent(this.props, e));
       }
     }
-  }
+  },
 });
