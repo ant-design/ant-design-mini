33e77a6da9a43bade41261bb322483c8c8a5021d
jc9702507 <jc9702507@gmail.com>
2022-11-25 17:26:04 +0800

    fix: switch

--- src/Switch/index.ts
+++ src/Switch/index.ts
@@ -1,29 +1,57 @@
 import { SwitchDefaultProps } from './props';
-import  fmtEvent from '../_util/fmtEvent';
-
+import fmtEvent from '../_util/fmtEvent';
+import { getValue } from '../_util/getValue';
 
 Component({
   props: SwitchDefaultProps,
   data: {
     selfChecked: undefined,
+    hasChange: false,
+    checkedInProps: undefined,
+  },
+  onInit() {
+    this.setCheckedInProps();
+  },
+  deriveDataFromProps(nextProps) {
+    if (nextProps.checked !== this.props.checked) {
+      this.setChange();
+    }
+  },
+  didUpdate(prevProps) {
+    if (prevProps.checked !== this.props.checked) {
+      this.setChange();
+    }
+  },
+  didMount() {
+    this.setCheckedInProps();
   },
   methods: {
-    onChange(e) {
-      if (this.props.disabled) {
+    setCheckedInProps() {
+      if (typeof this.data.checkedInProps !== 'undefined') {
         return;
       }
-      let checked: boolean;
-      if ('checked' in this.props) {
-        checked = !this.props.checked;
-      } else {
-        checked = !(typeof this.data.selfChecked !== 'undefined' ? this.data.selfChecked : this.props.defaultChecked);
+      this.setData({
+        checkedInProps: 'checked' in this.props,
+      });
+    },
+    setChange() {
+      if (!this.data.hasChange) {
         this.setData({
-          selfChecked: checked,
+          hasChange: true,
         });
       }
-      if (this.props.onChange) {
-        this.props.onChange(checked,  fmtEvent(this.props, e));
-      }
     },
-  },
+    onChange(e) {
+      const value = !getValue(this.data.checkedInProps, this.data.hasChange, this.props.checked, this.data.selfChecked, this.props.defaultChecked);
+      if (!this.data.checkedInProps) {
+        this.setData({
+          selfChecked: value,
+        });
+        this.setChange();
+      }
+      if (this.props.onChange) {
+        this.props.onChange(value, fmtEvent(this.props, e));
+      }
+    }
+  }
 });
