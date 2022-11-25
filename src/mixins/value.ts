068478a54aa1aaca04e3a87521c32fbc3820ef8f
jc9702507 <jc9702507@gmail.com>
2022-11-25 20:51:23 +0800

    feat: add mixin value

---
+++ src/mixins/value.ts
@@ -0,0 +1,54 @@
+export default ({
+  valueKey = 'value',
+  defaultValueKey = 'defaultValue',
+  scopeKey = 'mixin',
+  transformValue = value => ({
+    needUpdate: true,
+    value,
+  }),
+}: {
+  valueKey?: string;
+  defaultValueKey?: string;
+  scopeKey?: string;
+  transformValue?: (value: any) => {
+    needUpdate: boolean;
+    value?: any;
+  };
+} = {} as any) => {
+  return {
+    data: {
+      [scopeKey]: {
+        value: undefined,
+      },
+    },
+    didUpdate(prevProps) {
+      if (prevProps[valueKey] !== this.props[valueKey]) {
+        this.update(this.props[valueKey]);
+      }
+    },
+    didMount() {
+      const value = typeof this.props[valueKey] !== 'undefined' ? this.props[valueKey] : this.props[defaultValueKey];
+      this.update(value);
+    },
+    methods: {
+      getValue() {
+        return this.data[scopeKey].value;
+      },
+      isControlled() {
+        return [valueKey] in this.props;
+      },
+      update(val) {
+        const { needUpdate, value } = transformValue.call(this, val);
+        if (needUpdate) {
+          this.setData({
+            [`${scopeKey}.value`]: value,
+          });
+        }
+        return {
+          needUpdate,
+          value,
+        };
+      },
+    },
+  }
+};
