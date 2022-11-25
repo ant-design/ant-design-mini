245d9e52c18d7e2b2b4ef05fbc32819129104411
jc9702507 <jc9702507@gmail.com>
2022-11-25 20:55:33 +0800

    fix: swtich mixin value

--- src/Switch/index.ts
+++ src/Switch/index.ts
@@ -1,57 +1,22 @@
-import { SwitchDefaultProps } from './props';
+import { RadioDefaultProps } from './props';
 import fmtEvent from '../_util/fmtEvent';
-import { getValue } from '../_util/getValue';
+import mixinValue from '../mixins/value';
 
 Component({
-  props: SwitchDefaultProps,
-  data: {
-    selfChecked: undefined,
-    hasChange: false,
-    checkedInProps: undefined,
-  },
-  onInit() {
-    this.setCheckedInProps();
-  },
-  deriveDataFromProps(nextProps) {
-    if (nextProps.checked !== this.props.checked) {
-      this.setChange();
-    }
-  },
-  didUpdate(prevProps) {
-    if (prevProps.checked !== this.props.checked) {
-      this.setChange();
-    }
-  },
-  didMount() {
-    this.setCheckedInProps();
-  },
+  props: RadioDefaultProps,
+  mixins: [mixinValue({
+    valueKey: 'checked',
+    defaultValueKey: 'defaultChecked',
+  })],
   methods: {
-    setCheckedInProps() {
-      if (typeof this.data.checkedInProps !== 'undefined') {
-        return;
-      }
-      this.setData({
-        checkedInProps: 'checked' in this.props,
-      });
-    },
-    setChange() {
-      if (!this.data.hasChange) {
-        this.setData({
-          hasChange: true,
-        });
-      }
-    },
     onChange(e) {
-      const value = !getValue(this.data.checkedInProps, this.data.hasChange, this.props.checked, this.data.selfChecked, this.props.defaultChecked);
-      if (!this.data.checkedInProps) {
-        this.setData({
-          selfChecked: value,
-        });
-        this.setChange();
+      const value = !this.getValue();
+      if (!this.isControlled()) {
+        this.update(value);
       }
       if (this.props.onChange) {
         this.props.onChange(value, fmtEvent(this.props, e));
       }
     }
-  }
+  },
 });
