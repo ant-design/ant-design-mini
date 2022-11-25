8ebe959486be7318da7e03639e49d506d8b08efb
jc9702507 <jc9702507@gmail.com>
2022-11-25 21:01:02 +0800

    fix: radio mixin value

--- src/Radio/RadioGroup/index.ts
+++ src/Radio/RadioGroup/index.ts
@@ -1,46 +1,16 @@
 import { RadioGroupDefaultProps } from './props';
 import fmtEvent from '../../_util/fmtEvent';
-
+import mixinValue from '../../mixins/value';
 
 Component({
   props: RadioGroupDefaultProps,
-  data: {
-    selfValue: undefined,
-    hasChange: false,
-    valueInProps: undefined,
-  },
-  onInit() {
-    this.setValueInProps();
-  },
-  didMount() {
-    this.setValueInProps();
-  },
+  mixins: [mixinValue()],
   methods: {
-    setValueInProps() {
-      if (typeof this.data.valueInProps !== 'undefined') {
-        return;
-      }
-      this.setData({
-        valueInProps: 'value' in this.props,
-      });
-    },
-    setChange() {
-      if (this.data.hasChange) {
-        return;
-      }
-      this.setData({
-        hasChange: true,
-      });
-    },
     onChange(_, e) {
       const index = e.currentTarget.dataset.index;
       const value = this.props.options[index].value;
-      this.setChange();
-      if (!this.data.valueInProps) {  
-        this.setData({
-          hasChange: true,
-          selfValue: value,
-        });
+      if (!this.isControlled()) {
+        this.update(value);
       }
       if (this.props.onChange) {
         this.props.onChange(value, fmtEvent(this.props, e));
