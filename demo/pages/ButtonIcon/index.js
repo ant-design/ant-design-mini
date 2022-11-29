fbc1b8a6db2be9af6d48cc58496458edc4080017
jc <113670357+jc9702507@users.noreply.github.com>
2022-11-29 17:48:47 +0800

    fix: button (#384)
    * fix: button
    
    * fix: button props
    
    * fix: button
    
    * fix: button props
    
    * fix: button
    
    * fix: button disabled

--- demo/pages/ButtonIcon/index.js
+++ demo/pages/ButtonIcon/index.js
@@ -1,10 +1,3 @@
 Page({
-  data: {
-    url: 'https://gw.alipayobjects.com/mdn/rms_ce4c6f/afts/img/A*XMCgSYx3f50AAAAAAAAAAABkARQnAQ',
-  },
-  handleTap() {
-    my.alert({
-      title: 'tap',
-    });
-  },
+  
 });
--- src/Button/props.js
+++ src/Button/props.js
@@ -1,11 +1,4 @@
 export const ButtonDefaultProps = {
   type: 'default',
-  disabled: false,
-  inline: false,
-  inlineSize: 'medium',
-  loading: false,
   htmlType: 'button',
-  _submit: undefined,
-  fill: 'solid',
-  stopPropagation: false,
 };
