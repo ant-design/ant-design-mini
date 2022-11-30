5ac461bc3df65e46fd9dd80e3b109afaa56440d9
七柚 <30853720+wujunwen1997@users.noreply.github.com>
2022-11-30 14:33:14 +0800

    feat: review swipe action (#394)
    Co-authored-by: wujunwen <wujunwen.wjw@antgroup.com>

--- src/SwipeAction/index.ts
+++ src/SwipeAction/index.ts
@@ -1,16 +1,18 @@
 import { SwipeActionDefaultProps } from './props';
+import fmtEvent from '../_util/fmtEvent';
 
-const setStyleObj1 = (itemWidth: number) => [
+const setStyleObj1 = (buttons: any[], inertiaWidth?: number) => [
   { marginLeft: 0 },
-  { marginLeft: `${-(itemWidth + 1) / 2}px` },
-  { marginLeft: `${-(2 * itemWidth + 1) / 2}px` }
+  { marginLeft: `${-(buttons[0]?.width + 1 + (inertiaWidth || 0)) / 2}px` },
+  { marginLeft: `${-(buttons[0]?.width + buttons[1]?.width + 1 + (inertiaWidth || 0)) / 2}px` }
 ];
-const setStyleObj2 = (itemWidth: number, length) => {
+const setStyleObj2 = (buttons: any[], inertiaWidth?: number) => {
+  const length = buttons.length;
   if (length === 2) {
-    return [ { marginRight: 0 }, { marginRight: `${-itemWidth / 2}px` } ]
+    return [ { marginRight: 0 }, { marginRight: `-${(buttons[0]?.width + (inertiaWidth || 0)) / 2}px` } ]
   }
   if (length === 3) {
-    return [ { marginRight: 0 }, { marginRight: `${-itemWidth / 2}px` }, { marginRight: `${-itemWidth}px` } ]
+    return [ { marginRight: 0 }, { marginRight: `-${(buttons[0]?.width + (inertiaWidth || 0))/ 2}px` }, { marginRight: `-${((buttons[0]?.width + buttons[1]?.width + (inertiaWidth || 0))) / 2}px` } ]
   }
   return [{ marginRight: 0 }]
 }
@@ -44,45 +46,57 @@ Component({
     inertiaWidth: 20,
   },
   didMount() {
-    const { itemWidth, right, left, rightSwiped, leftSwiped, inertia } = this.props;
-    const _rightArr = right || [];
-    const _leftArr = left || [];
-    this.setData({
-      rightWidth: itemWidth * _rightArr.length,
-      leftWidth: itemWidth * _leftArr.length,
-      inertiaWidth: inertia ? 20 : 0,
-    });
-    if (rightSwiped || leftSwiped) {
+    const { defaultSwiped, elasticity } = this.props;
+    this.setButtonItemWidth();
+    this.setData({ inertiaWidth: elasticity ? 20 : 0 });
+    if (defaultSwiped) {
       this.initWidth((maxSwipe: any) => {
         maxSwipe && this.setData({
-          swipeX: (maxSwipe + 0.01) * (!!rightSwiped ? -1 : 1),
-          swipedR: rightSwiped && !leftSwiped,
-          swipedL: leftSwiped && !rightSwiped,
+          swipeX: (maxSwipe + 0.01) * (defaultSwiped === 'right' ? -1 : 1),
+          swipedR: defaultSwiped === 'right',
+          swipedL: defaultSwiped === 'left',
         });
       });
     }
   },
   didUpdate(prevProp) {
-    const { rightSwiped, leftSwiped, inertia, damping } = this.props;
+    const { swiped, damping, elasticity } = this.props;
     // 设置不同的滑动位置时需要重置
-    const rs = prevProp.rightSwiped !== rightSwiped && !rightSwiped;
-    const ls = prevProp.leftSwiped !== leftSwiped && !leftSwiped;
-    const is = prevProp.inertia !== inertia;
+    const rs = prevProp.swiped !== swiped;
+    const is = prevProp.elasticity !== elasticity;
     const ds = prevProp.damping !== damping;
-    if (rs || ls || is || ds) {
+    if (rs || is || ds) {
       this.setData({ swipeX: 0, swipedR: false, swipedL: false, tapTypeL: '', tapTypeR: '' });
     }
     if (is) {
-      this.setData({ inertiaWidth: inertia ? 20 : 0 });
+      this.setData({ inertiaWidth: elasticity ? 20 : 0 });
     }
   },
   methods: {
+    setWidth() {
+      const { leftButtons, rightButtons } = this.props;
+      const _rightArr = rightButtons || [];
+      const _leftArr = leftButtons || [];
+      this.setData({
+        rightWidth: _rightArr.reduce((tolal, cur) => { return parseFloat(tolal) + cur.width }, 0),
+        leftWidth: _leftArr.length ? _leftArr.reduce((tolal, cur) => { return parseFloat(tolal) + cur.width }, 0) : 0,
+      });
+    },
+    setButtonItemWidth() {
+      const { leftButtons, rightButtons } = this.props;
+      leftButtons.forEach(i => {
+        i.width = i.width || 150
+      });
+      rightButtons.forEach(i => {
+        i.width = i.width || 150
+      });
+      this.setWidth();
+    },
     initWidth(func?: Function) {
-      const { itemWidth, right, left } = this.props;
-      this.setData({ rightWidth: itemWidth * right.length, leftWidth: itemWidth * left.length });
+      const { leftButtons, rightButtons } = this.props;
       // 获取宽度信息，设置滑轨的宽度、初始化滑动位置
       // 如果没有获取到该信息则把左滑禁用掉
-      right.length > 0 && my.createSelectorQuery()
+      rightButtons.length > 0 && my.createSelectorQuery()
         .select(`.paytm-swipe-movable-right-${this.$id}`)
         .boundingClientRect()
         .exec((ret: any) => {
@@ -91,7 +105,7 @@ Component({
             func && func(ret[0].width);
           }
         });
-        left.length > 0 && my.createSelectorQuery()
+        leftButtons.length > 0 && my.createSelectorQuery()
           .select(`.paytm-swipe-movable-left-${this.$id}`)
           .boundingClientRect()
           .exec((ret: any) => {
@@ -103,15 +117,13 @@ Component({
     },
     // 向外透出事件
     onTouchStart() {
-      const { onSwipeStart, key, callbackData } = this.props;
+      const { onSwipeStart } = this.props;
       this.initWidth();
-      this.setData({ tapTypeL: '', tapTypeR: '', inTouch: true }); // 清空eventType = auto 的表现
-      onSwipeStart(key, callbackData);
+      this.setData({ tapTypeL: '', tapTypeR: '', inTouch: true }); // 清空confirmType = auto 的表现
+      onSwipeStart(fmtEvent(this.props));
     },
     onTouchEnd() {
-      const { onTouchEnd, key, callbackData } = this.props;
       this.setData({ inTouch: false });
-      onTouchEnd(key, callbackData);
     },
     // 滑动过程中的事件，是内部事件不向外透出，用于控制右侧按钮的位置信息
     onChange(e: any) {
@@ -122,8 +134,8 @@ Component({
       const newArr = changeArr[1] === L ? [changeArr] : [changeArr[1], L];
       this.setData({ moveX: L, changeArr: newArr });
 
-      const ridx = this.props.right.findIndex((u) => u.eventType === 'move');
-      const lidx = this.props.left.findIndex((u) => u.eventType === 'move');
+      const ridx = this.props.rightButtons.findIndex((u) => u.confirmType === 'move');
+      const lidx = this.props.leftButtons.findIndex((u) => u.confirmType === 'move');
       if (ridx === -1 && lidx === -1)  return;
       let isRight = getDirectionLeft(changeArr);
       // 左滑时的滑动确认、收起处理
@@ -158,15 +170,16 @@ Component({
       }
     },
     onSetCheck(isRight: boolean) {
-      const { right, itemWidth, left } = this.props;
-      const arr = isRight ? right : left;
-      const idx = arr.findIndex((u) => u.eventType === 'move');
+      const { rightButtons, leftButtons } = this.props;
+      const { inertiaWidth } = this.data;
+      const arr = isRight ? rightButtons : leftButtons;
+      const idx = arr.findIndex((u) => u.confirmType === 'move');
       if (idx === -1)  return;
       my.vibrateShort({ success() {} });
-      const styArr = isRight ? setStyleObj1(itemWidth) : setStyleObj2(itemWidth, left.length);
+      const styArr = isRight ? setStyleObj1(rightButtons, inertiaWidth) : setStyleObj2(leftButtons);
       const sty = styArr[idx];
       this.setData({ myStyle: sty });
-      isRight ? this.setData({ tapTypeR: arr[idx].type }) : this.setData({ tapTypeL: arr[idx].type });
+      isRight ? this.setData({ tapTypeR: 'R-' + idx }) : this.setData({ tapTypeL: 'L-' + idx });
     },
     // 意外中断了滑动，则立即开始结算滑动动作
     onTouchCancel(e: any) {
@@ -184,7 +197,7 @@ Component({
     },
     onSetSwipeRight(x: number, needBack?: boolean) {
       const { swipedR, inTouch, tapTypeR, changeArr } = this.data;
-      const { onRightItemEvent, key, callbackData } = this.props;
+      const { onButtonTap } = this.props;
       let isRight = Math.abs(changeArr[1]) > Math.abs(changeArr[0]);
       if (!swipedR && Math.abs(x) < 10) {
         isRight = false;
@@ -193,7 +206,7 @@ Component({
       !isRight && this.setData({ tapTypeR: '', myStyle: {} });
       if (inTouch && !!tapTypeR) {
         this.setData({ tapTypeR: '', myStyle: {} });
-        onRightItemEvent(tapTypeR, key, callbackData);
+        onButtonTap(fmtEvent(this.props), { direction: 'right', btnIdx: tapTypeR.replace('R-', '')});
         this.onSwipeRight(false);
         return;
       }
@@ -202,7 +215,7 @@ Component({
     },
     onSetSwipeLeft(x: number, needBack?: boolean) {
       const { swipedL, inTouch, tapTypeL, changeArr } = this.data;
-      const { onRightItemEvent, key, callbackData } = this.props;
+      const { onButtonTap } = this.props;
       // true：初始是往右滑，false: 右滑收起
       let isRight = changeArr[1] >= changeArr[0];
       if (!swipedL && Math.abs(x) < 10) {
@@ -214,7 +227,7 @@ Component({
       // 处理滑动-触发事件
       if (inTouch && !!tapTypeL) {
         this.setData({ tapTypeL: '', myStyle: {} });
-        onRightItemEvent(tapTypeL, key, callbackData);
+        onButtonTap(fmtEvent(this.props), { direction: 'left', btnIdx: tapTypeL.replace('L-', '') });
         this.onSwipeLeft(false);
         return;
       }
@@ -224,7 +237,7 @@ Component({
     },
     // 结算
     onSwipeLeft(isRight: boolean) {
-      const { onSwipeEnd, key, callbackData } = this.props;
+      const { onSwipeEnd } = this.props;
       const { maxSwipeL, inertiaWidth } = this.data;
       // 为了处理到重置状态的效果
       const maxX = (maxSwipeL + 0.01 + inertiaWidth);
@@ -241,7 +254,7 @@ Component({
                 swipedL: !flag,
               },
               () => {
-                onSwipeEnd(key, { direction: isRight ? 'right' : 'left', left: true }, callbackData);
+                onSwipeEnd(fmtEvent(this.props), { direction: 'left', swiped: !!isRight });
               },
             );
           }, inertiaWidth > 0 ? 200 : 0)
@@ -250,7 +263,7 @@ Component({
     },
     onSwipeRight(isRight: boolean) {
       const { maxSwipeR, inertiaWidth } = this.data;
-      const { onSwipeEnd, key, callbackData } = this.props;
+      const { onSwipeEnd } = this.props;
       // 处理x的位置，两次setData X是因为x如果相同位置不会变，刚好也把弹性在这里处理了
       const _this = this;
       // 为了处理到重置状态的效果
@@ -261,57 +274,49 @@ Component({
         },
         () => {
           const flag = _this.data.swipeX === -0.01;
-          inertiaWidth > 0 ? setTimeout(() => {
+          setTimeout(() => {
             this.setData(
               {
                 swipeX: flag ? 0 : -(maxSwipeR + 0.01),
                 swipedR: !flag,
               },
               () => {
-                onSwipeEnd(key, { direction: isRight ? 'left' : 'right', right: true }, callbackData);
+                onSwipeEnd(fmtEvent(this.props), { direction: 'right', swiped: !!isRight });
               },
             );
-          }, 200) : this.setData(
-            {
-              swipeX: flag ? 0 : -(maxSwipeR + 0.01),
-              swipedR: !flag,
-            },
-            () => {
-              onSwipeEnd(key, { direction: isRight ? 'left' : 'right', right: true }, callbackData);
-            },
-          );
+          },  inertiaWidth > 0 ? 200 : 0);
         },
       );
     },
     // 处理右侧点击事件
     onItemTap(e: any) {
       const { swipeLeft, tapTypeL, tapTypeR } = this.data;
-      const { onRightItemEvent, callbackData, itemWidth, key, onLeftItemEvent, left } = this.props;
-      if (!onRightItemEvent && !onLeftItemEvent) return;
+      console.log(e);
+      const { onButtonTap, leftButtons, rightButtons } = this.props;
+      if (!onButtonTap) return;
       const { itemL, idx, item } = e.target.dataset.item;
-      const { eventType, type } = swipeLeft ? item : itemL;
-      const styArr = swipeLeft ? setStyleObj1(itemWidth) : setStyleObj2(itemWidth, left.length);
-      const sty = styArr[idx];
-      if (tapTypeL === type) {
+      const { confirmType } = swipeLeft ? item : itemL;
+      if (tapTypeL === ('L-' + idx)) {
         this.onSetSwipeLeft(0, true);
         this.setData({ tapTypeL: '', myStyle: {} });
-        onLeftItemEvent(type, key, callbackData);
+        onButtonTap(fmtEvent(this.props, { direction: 'left', btnIdx: idx }));
         return;
       }
-      if (tapTypeR === type) {
+      if (tapTypeR === ('R-' + idx)) {
         this.onSetSwipeRight(0, true);
         this.setData({ tapTypeR: '', myStyle: {} });
-        onRightItemEvent(type, key, callbackData);
+        onButtonTap(fmtEvent(this.props, { direction: 'right', btnIdx: idx }));
         return;
       }
       // auto 是展开按钮二次确认的效果
-      if (eventType === 'auto' || eventType === 'move') {
+      if (confirmType === 'tap' || confirmType === 'move') {
+        const styArr = swipeLeft ? setStyleObj1(rightButtons) : setStyleObj2(leftButtons);
+        const sty = styArr[idx];
         this.setData({ myStyle: sty });
-        !swipeLeft ? this.setData({ tapTypeL: type }) :  this.setData({ tapTypeR: type });
+        !swipeLeft ? this.setData({ tapTypeL: 'L-' + idx }) :  this.setData({ tapTypeR: 'R-' + idx });
       } else {
         !swipeLeft ? this.onSetSwipeLeft(0, true) : this.onSetSwipeRight(0, true);
-        const event = swipeLeft ? onRightItemEvent : onLeftItemEvent;
-        event(type, key, callbackData);
+        onButtonTap(fmtEvent(this.props), { direction: !swipeLeft ? 'left' : 'right', btnIdx: idx  });
       }
     },
   },
--- src/SwipeAction/props.d.ts
+++ src/SwipeAction/props.d.ts
@@ -1,19 +1,16 @@
 import { IBaseProps } from '../_base';
-
 interface SwipeButton {
-    type: string,
-    text: string,
-    bgColor?: string,
-    color?: string,
-    style?: object,
-    eventType?: 'move' | 'auto',
-    confirmText?: string,
+  text: string,
+  width?: number,
+  bgColor?: string,
+  color?: string,
+  confirmType?: '' | 'move' | 'tap',
+  confirmText?: string,
 }
-
-interface SwipeEnd {
-    direction: 'right' | 'left',
-    left?: boolean,
-    right?: boolean,
+interface SwipeData {
+  direction: 'right' | 'left',
+  btnIdx?: number,
+  swiped?: boolean,
 }
 
 interface ISwipeActionProps extends IBaseProps  {
@@ -21,71 +18,48 @@ interface ISwipeActionProps extends IBaseProps  {
    * @description 左侧滑出事件
    * @default []
    */
-   left?: SwipeButton[];
+  leftButtons?: SwipeButton[];
   /**
    * @description 右侧滑出事件
    * @default []
    */
-   right?: SwipeButton[];
-  /**
-   * @description 滑出按钮宽度
-   * @default 150
-   */
-   itemWidth?: number;
+  rightButtons?: SwipeButton[];
   /**
    * @description 滑动速度
-   * @default 20
+   * @default 60
    */
-   damping?: number;
-  /**
-   * @description 唯一标识
-   * @default ''
-   */
-   key: string;
-  /**
-   * @description 滑动弹性
-   * @default true
-   */
-   inertia?: boolean;
-  /**
-   * @description 左侧按钮是否滑开
-   * @default false
-   */
-   leftSwiped?: boolean;
-  /**
-   * @description 右侧按钮是否滑开
-   * @default false
-   */
-   rightSwiped?: boolean;
+  damping?: number;
   /**
    * @description 禁止滑动
    * @default false
    */
-   disable?: boolean;
+  disable?: boolean;
   /**
-   * @description 回调数据
-   * @default {}
+   * @description 禁止滑动
+   * @default ''
    */
-   callbackData?: object;
+  swiped?: '' | 'left' | 'right';
+  /**
+   * @description 禁止滑动
+   * @default ''
+   */
+  defaultSwiped?: '' | 'left' | 'right';
+  /**
+   * @description 弹性超出最大滑动距离自动弹回
+   * @default true
+   */
+  elasticity?: boolean
   /**
    * @description 滑动结束
    */
-   onSwipeEnd: (key: string, swipeObj: SwipeEnd, callbackData: object) => void;
+  onSwipeEnd: (e: Record<string, any>, data: SwipeData) => void;
   /**
    * @description 滑动、触摸开始
    */
-   onSwipeStart: (key: string, callbackData: object) => void;
-  /**
-   * @description 触摸结束
-   */
-   onTouchEnd: (key: string, callbackData: object) => void;
+  onSwipeStart: (e: Record<string, any>) => void;
   /**
    * @description 右侧滑动按钮的点击事件回调
    */
-   onRightItemEvent: (type: string, key: string, callbackData: object) => void;
-  /**
-   * @description 左侧滑动按钮的点击事件回调
-   */
-   onLeftItemEvent: (type: string, key: string, callbackData: object) => void;
+  onButtonTap: (e: Record<string, any>, data: SwipeData) => void;
 }
 export declare const SwipeActionDefaultProps: Partial<ISwipeActionProps>;
