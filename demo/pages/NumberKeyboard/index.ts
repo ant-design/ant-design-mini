1b43f3413ab53b0a0500cff54b6b05a9acb9928b
wujunwen <wujunwen.wjw@antgroup.com>
2022-12-08 14:20:08 +0800

    feat: 调整数字键盘

--- demo/pages/NumberKeyboard/index.ts
+++ demo/pages/NumberKeyboard/index.ts
@@ -1,4 +1,11 @@
 Page({
-  data: {},
+  data: {
+    visible: true,
+    value1: '',
+  },
   onLoad() {},
+  onInput(e) {
+    console.log('gaibain', e);
+    this.setData({ value1: e });
+  }
 });
---
+++ src/NumberKeyboard/index.ts
@@ -0,0 +1,81 @@
+// import { VirtualKeyboardDefaultProps } from './props';
+
+Component({
+  props: {
+    value: '', // 值
+    defaultValue: '', // 默认值
+    visible: false, // 是否展示
+    safeArea: true, // 安全区域
+    arrow: false, // 隐藏箭头
+    random: false, // 乱序
+    vibrate: false, // 震动反馈
+    point: true, // 展示小数点
+    disable: false, // 禁用确认按钮
+    confirmText: '确定', // 确认按钮文字
+    controlled: true, // 是否受控
+    onInput: ()=> {}, // 输入
+    onConfirm: ()=> {}, // 确认
+    onDelete: (val)=> {}, // 删除
+    onClose: () => {}, // 关闭
+  },
+  data: {
+    _val: '',
+    _visible: false,
+    _disable: false,
+  },
+  didMount() {
+    const { value, visible } = this.props;
+    this.setData({
+      _val: value,
+      _visible: visible,
+    });
+  },
+  didUpdate(prevProps) {
+    const { value, visible } = this.props;
+    if (visible !== prevProps.visible) {
+      this.setData({ _visible: visible });
+      !visible && this.onHide();
+    }
+    // if (value !== prevProps.value) {
+    //   console.log('213213')
+    //   this.setData({ _val: value });
+      
+    // }
+  },
+  methods: {
+    tapButton(e) {
+      const { controlled, value } = this.props;
+      this.vibrate();
+      const _key = e.target.dataset.key;
+      const _val = controlled ? value : this.data._val.toString();
+      console.log('?', this.props.value);
+      console.log('*', `${_val}${_key}`);
+      
+      const { onInput, onDelete } = this.props;
+      // 回退
+      if (_key === 'del') {
+        onDelete(this.data._val );
+        // onInput(this.data._val);
+        return;
+      }
+      _key !== 'del' && _key !== 'enter' && onInput(`${_val}${_key}` );
+      // _key === 'enter' && this.onClickEnter()
+    },
+    // 隐藏键盘，失去焦点
+    onHide() {
+      const { onClose } = this.props;
+      this.setData({ _focus: false });
+      onClose();
+    },
+    onClickEnter() {
+      const { disable, onConfirm } = this.props;
+      if (disable) return;
+      this.onHide();
+      onConfirm();
+    },
+    // 振动反馈
+    vibrate() {
+      this.props.vibrate && my.canIUse('vibrateShort') && my.vibrateShort();
+    },
+  },
+});
