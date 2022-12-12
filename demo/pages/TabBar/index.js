7a55c03c9f719ca6e204317b797c746670c02954
wyj580231 <30888500+wyj580231@users.noreply.github.com>
2022-12-12 10:55:18 +0800

    1.0 wyj new (#426)
    * feat: 调整Piker、DatePicker demo
    
    * feat: 调整popover demo
    
    * feat: 调整GuideTour  demo
    
    * feat: 调整Tabbar demo
    
    Co-authored-by: wyj <wb-wyj631405@antfin.com>

--- demo/pages/TabBar/index.js
+++ demo/pages/TabBar/index.js
@@ -38,26 +38,6 @@ Page({
       },
     ],
 
-    tabsImage: [
-      {
-        icon: 'AlipayCircleFill',
-        activeIcon: 'AlipayCircleFill',
-        text: '首页',
-      },
-      {
-        icon: 'https://gw.alipayobjects.com/mdn/industry_lifee/afts/img/A*rF7uRZrUpiEAAAAAAAAAAAAAARQnAQ',
-        activeIcon:
-          'https://gw.alipayobjects.com/mdn/industry_lifee/afts/img/A*a29CQa2-9VIAAAAAAAAAAAAAARQnAQ',
-        text: '导航',
-      },
-      {
-        icon: 'https://gw.alipayobjects.com/mdn/industry_lifee/afts/img/A*lZCJTa_1JSYAAAAAAAAAAAAAARQnAQ',
-        activeIcon:
-          'https://gw.alipayobjects.com/mdn/industry_lifee/afts/img/A*W7e3R58X0XkAAAAAAAAAAAAAARQnAQ',
-        text: '我的',
-      },
-    ],
-
     tabsCount6: [
       {
         icon: 'AlipayCircleFill',
