acb4a76acf7bf6899f2ae1773fed8294c5068e6d
七柚 <30853720+wujunwen1997@users.noreply.github.com>
2022-12-13 14:54:32 +0800

    Feat/indexbar (#432)
    * feat: Indexbar 索引组件基本完成
    
    * feat: 删除bv组件
    
    * feat: indexbar ci fixed
    
    * feat: 调整indexbar的props
    
    * feat: 调整indexbar组件
    
    * feat: 索引组件完成
    
    * feat: 删除indexbar中的scrollIntoView属性
    
    * feat: 删除多余样式
    
    * feat: 完成indexbar组件
    
    * feat: 禁止indexbar组件下拉刷新能力
    
    Co-authored-by: wujunwen <wujunwen.wjw@antgroup.com>

---
+++ src/Indexbar/props.js
@@ -0,0 +1,10 @@
+export const IndexbarDefaultProps = {
+  className: '',
+  activeClassName: '',
+  defaultCurrent: '',
+  current: '',
+  vibrate: true,
+  items: [],
+  size: 16,
+  onChange: (value, index) => {},
+}
