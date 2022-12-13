3cf5d8a524465b75bb91425342654f50f0091c31
wujunwen <wujunwen.wjw@antgroup.com>
2022-12-13 11:10:44 +0800

    feat: 增加swipe-action实例

---
+++ demo/pages/SwipeActionLeft/index.ts
@@ -0,0 +1,29 @@
+Page({
+  data: {
+    rightBtns: [
+      {
+        text: '往来记录',
+        bgColor: '#1677FF',
+        color: '#fff',
+      },
+      {
+        text: '设为常用',
+        bgColor: '#FFA91B',
+        color: '#fff',
+      },
+      {
+        text: '删除',
+        bgColor: '#FF2B00',
+        color: '#fff',
+      }
+    ],
+    swipeIndex: -1
+  },
+  onSwipeStart() {
+    this.setData({ swipeIndex: '' });
+  },
+  onSwipeEnd(e, data) {
+    const { index } = e.target.dataset.item;
+    data.swiped && this.setData({ swipeIndex: index });
+  },
+});
