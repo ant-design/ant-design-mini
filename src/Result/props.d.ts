873a91dcd9ce54f5f1ab5a56d708660f1b3b41d7
wyj <wb-wyj631405@antfin.com>
2022-12-15 17:10:45 +0800

    feat: Result slot 调整

--- src/Result/props.d.ts
+++ src/Result/props.d.ts
@@ -1,4 +1,4 @@
-import { IBaseProps, IconType } from '../_base';
+import { IBaseProps } from '../_base';
 
 /**
  * @description 结果页，用于展示用户操作后的信息反馈。
@@ -13,7 +13,7 @@ export interface IResultProps extends IBaseProps {
   /**
    * @description 自定义图片，如果配置了 type， 则不生效
    */
-  image?: IconType;
+  image?: string;
 
   /**
    * @description 主文案
@@ -24,10 +24,5 @@ export interface IResultProps extends IBaseProps {
    * @description 副文案
    */
   message?: string;
-
-  /**
-   * @description 按钮点击回调
-   */
-  onButtonTap?: (index: number) => void;
 }
 export declare const ResultDefaultProps: Partial<IResultProps>;
