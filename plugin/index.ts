6c542a7c84f44a886cba43e4eaf7a017adc895c4
墨帆 <gongxiangqian.gxq@antgroup.com>
2023-02-01 21:48:29 +0800

    feat: 更新openbox版本&升级demo功能

--- plugin/index.ts
+++ plugin/index.ts
@@ -12,9 +12,12 @@ export default (api: IApi) => {
       component: path.join(__dirname, './render'),
       transformer(opts) {
         if (opts.attrs.src.startsWith('pages')) {
+          const pageElem = opts.attrs.src.split('/');
+          pageElem.pop();
+          const folder = pageElem.join('/') + '/';
           return {
             previewerProps: {
-              herboxUrl: `/preview.html?defaultPage=${opts.attrs.src}&defaultOpenedFiles=${opts.attrs.src}&theme=light&compilerServer=${process.env.SERVER || ''}&noChangeButton=${opts.attrs.nochangebutton || ''}`
+              herboxUrl: `/preview.html?page=${opts.attrs.src}&folder=${folder}&theme=light&compilerServer=${process.env.SERVER || ''}&noChangeButton=${opts.attrs.nochangebutton || ''}`
             },
           };
         }
@@ -45,30 +48,64 @@ export default (api: IApi) => {
 function getFileContent(file) {
   return fs.promises.readFile(file, 'utf-8');
 }
-
-async function getSourceCode(page: string) {
-  const map = {
-    '.js': getFileContent,
-    '.axml': getFileContent,
-    '.acss': getFileContent,
-    '.less': lessCompile,
-    '.json': getFileContent,
-  };
-  const list: Promise<string>[] = [];
-  const arr: string[] = [];
-  Object.keys(map).forEach(item => {
-    const filename = path.join(__dirname, '../demo', page + item);
-    if (fs.existsSync(filename)) {
-      arr.push(page + (item === '.less' ? '.acss' : item));
-      list.push(map[item](filename));
+  async function readAllFileFromDir(dirPath) {
+    const allFileList: string[] = [];
+    async function recursion(dir) {
+        try {
+            const files = await fs.promises.readdir(dir);
+            for (const file of files) {
+                // 获取当前文件路径
+                const joinPath = path.join(dir, file);
+                // 判断文件属性
+                const stat = await fs.promises.stat(joinPath);
+                // 如果是文件夹，则递归
+                if (stat.isDirectory()) {
+                    await recursion(joinPath)
+                // 否则加入 list
+                } else {
+                    allFileList.push(joinPath);
+                }
+            }
+        } catch (err) {
+            console.error(err);
+        }
     }
-  });
-  const json = {};
-  (await Promise.all(list)).forEach((item, index) => {
-    json[arr[index]] = item.replace(/('|")[^'"]*\/src/g, '$1antd-mini/es');
-  });
-  return json;
-}
+    // 开始
+    await recursion(dirPath);
+    return allFileList;
+  }
+
+  async function getSourceCode(page: string) {
+    // 读取文件夹中的路径
+    const dirList = await readAllFileFromDir(path.join(__dirname, '../demo', page));
+
+    const map = {
+      '.js': getFileContent,
+      '.axml': getFileContent,
+      '.acss': getFileContent,
+      '.less': lessCompile,
+      '.json': getFileContent,
+      '.sjs': getFileContent
+    };
+
+    const list: Promise<string>[] = [];
+    const arr: string[] = [];
+
+    dirList.forEach(fileName => {
+      const splitArr = fileName.split('.');
+      const ext = splitArr[splitArr.length - 1];
+      arr.push(ext === 'less' ? fileName.replace('.less', '.acss') : fileName);
+      list.push(map[`.${ext}`](fileName));
+    });
+
+
+    const json = {};
+    (await Promise.all(list)).forEach((item, index) => {
+      const n = arr[index].replace(path.join(__dirname, '../demo/'), '');
+      json[n] = item.replace(/('|")[^'"]*\/src/g, '$1antd-mini/es');
+    });
+    return json;
+  }
 
 
 function lessCompile(filename: string) {
