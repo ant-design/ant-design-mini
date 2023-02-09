8eeb02eafb0a4d6ec9fb6f48d7be611665e5c953
wyj <wangyuejun86@126.com>
2023-02-09 17:10:59 +0800

    docs: update CheckboxCustomGroup demo

--- demo/pages/CheckboxCustomGroup/index.js
+++ demo/pages/CheckboxCustomGroup/index.js
@@ -1,5 +1,41 @@
 Page({
   data: {
+    options: [
+      {
+        title: '分组一',
+        list: [
+          {
+            title: '选项一',
+            value: '1',
+          },
+          {
+            title: '选项二',
+            value: '2',
+          },
+          {
+            title: '选项三',
+            value: '3',
+          },
+        ],
+      },
+      {
+        title: '分组二',
+        list: [
+          {
+            title: '选项四',
+            value: '4',
+          },
+          {
+            title: '选项五',
+            value: '5',
+          },
+          {
+            title: '选项六',
+            value: '6',
+          },
+        ],
+      },
+    ],
     value: [],
   },
   onChange(checked, e) {
