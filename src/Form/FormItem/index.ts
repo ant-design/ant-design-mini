c6c078f5e10309b94f6f9f16928ff349738da111
彧衡 <yuheng.lh@antgroup.com>
2023-02-09 10:46:24 +0800

    chore(formItem): 删除formItem无用代码

--- src/Form/FormItem/index.ts
+++ src/Form/FormItem/index.ts
@@ -2,15 +2,5 @@ import { FormItemDefaultProps } from './props';
 
 Component({
   props: FormItemDefaultProps,
-  data: {
-    helpVisible: false,
-  },
-  methods: {
-    onHelpVisibleChange(visible: boolean) {
-      this.setData({
-        helpVisible: visible,
-      });
-    }
-  }
 });
 
