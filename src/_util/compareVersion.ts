27e95116ed9059f2087db00d95058753933f41e8
赤璃 <lubinghao.lbh@alibaba-inc.com>
2023-04-19 15:12:19 +0800

    feat: 增强sdkversion比较逻辑

--- src/_util/compareVersion.ts
+++ src/_util/compareVersion.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
-export function compareVersion(v1: string, v2: string): number {
+export function compareVersion(v1: string | undefined, v2: string): number {
   if (v1 === v2) return 0;
+  if (v1 === undefined) return 0;
   const v1Arr = v1.split('.');
   const v2Arr = v2.split('.');
   const len = v1Arr.length < v2Arr.length ? v1Arr.length : v2Arr.length;
