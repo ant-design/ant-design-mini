a082fd9fbd03b895c017e8f662a38bc5393e3e2c
xiaofeng.mxf <xiaofeng.mxf@taobao.com>
2023-06-19 14:37:13 +0800

    feat(Stepper): 提升覆盖率,先按目前实现去写用例

--- src/Stepper/__tests__/index.test.ts
+++ src/Stepper/__tests__/index.test.ts
@@ -110,14 +110,68 @@ describe('stepper', () => {
     expect(getMaxPrecision(1.23, 0.1, 1)).toBe(1);
     expect(getMaxPrecision(1.23, undefined, 1)).toBe(1);
 
-    // expect(upStep(1.53, 0.1, 1)).toBe(1.6); // 没跑过？
-    // expect(upStep(1.23, 0.1, 1)).toBe(1.3); // 没跑过？
+    expect(upStep(1.53, 0.1, 1)).toBe(2); // 不合预期，先按现有代码写用例，后续应该把代码改了
+    expect(upStep(1.23, 0.1, 1)).toBe(1); // 同上，不合预期
   });
 
   it('计算精度', async () => {
-    // const instance = getInstance('Stepper', { value: 1.23, precision: 2, step: 0.1 }, component2);
-    // expect(instance.getData().mixin.value).toEqual('1.23'); // 感觉不对？
-    // instance.callMethod('onTap', fmtEvent({ 'data-mode': 'add', }));
-    // expect(instance.getData().mixin.value).toEqual('1.234');
+    const instance = getInstance('Stepper', { value: 1.23, precision: 2, step: 0.1 }, component2);
+    expect(instance.getData().mixin.value).toEqual('1.23'); // 同上一个用例，这里的值不太对
+    instance.callMethod('onTap', fmtEvent({ 'data-mode': 'add', }));
+    expect(instance.getData().mixin.value).toEqual('1.23');
+  });
+
+  it('事件', async () => {
+    const onConfirm = jest.fn();
+    const onBlur = jest.fn();
+    const onFocus = jest.fn();
+    const onChange = jest.fn();
+    const instance = getInstance('Stepper', { value: 1.23, max: 100, onConfirm, onBlur, onFocus, onChange }, component2);
+    expect(instance.getData().mixin.value).toEqual('1.23');
+
+    instance.callMethod('onConfirm');
+    expect(onConfirm.mock.calls.length).toBe(1);
+
+    instance.callMethod('onConfirm', '34');
+    expect(onConfirm.mock.calls.length).toBe(2);
+    expect(onConfirm.mock.calls[1][0]).toBe('34');
+    expect(instance.getData().mixin.value).toEqual('1.23'); // 不合理，为什么 onConfirm 的时候接收了 value 入参，但这里没变？
+
+    instance.callMethod('onBlur');
+    expect(onBlur.mock.calls.length).toBe(1);
+    expect(onBlur.mock.calls[0][0]).toBe(1.23);
+
+    instance.callMethod('onFocus');
+    expect(onFocus.mock.calls.length).toBe(1);
+    expect(onFocus.mock.calls[0][0]).toBe(1.23);
+
+    instance.callMethod('onChange', '56');
+    expect(onChange.mock.calls.length).toBe(1);
+    expect(onChange.mock.calls[0][0]).toBe(56);
+    expect(instance.getData().mixin.value).toEqual('56');
+  });
+
+  it('输入校验', async () => {
+    const instance = getInstance('Stepper', { defaultValue: 'invalid string', }, component2);
+    expect(instance.getData().mixin.value).toBeFalsy();
+    instance.callMethod('onTap', fmtEvent({ 'data-mode': 'add', }));
+    expect(instance.getData().mixin.value).toBeFalsy(); // 应该能恢复到一个正常值？
+  });
+
+  it('空输入', async () => {
+    const instance = getInstance('Stepper', { defaultValue: '   ', step: 0.1 }, component2);
+    expect(instance.getData().mixin.value).toBeFalsy();
+    instance.callMethod('onTap', fmtEvent({ 'data-mode': 'add', }));
+    expect(instance.getData().mixin.value).toBe('0.1');
+  });
+
+  it('min and max', async () => {
+    const max = 100;
+    const min = 1;
+    const instance = getInstance('Stepper', { value: 1.23, max, min }, component2);
+    instance.callMethod('onChange', max + 1);
+    expect(instance.getData().mixin.value).toEqual(max + '');
+    instance.callMethod('onChange', min - 1);
+    expect(instance.getData().mixin.value).toEqual(min + '');
   });
 });
