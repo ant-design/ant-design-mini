d24ec9db8759cfc50a28052729b6bd89e78f3340
EAGzzyCSL <eagzzycsl@hotmail.com>
2023-07-18 10:11:40 +0800

    fix: missing FormTextareaDefaultProps (#791)
    LGTM

--- src/Form/FormTextarea/props.ts
+++ src/Form/FormTextarea/props.ts
@@ -5,7 +5,7 @@ import { TextareaProps } from '../../Input/Textarea/props';
 export interface FormTextareaProps extends Omit<TextareaProps, 'value' | 'defaultValue'>, FormItemProps {
 }
 
-export declare const FormTextareaDefaultProps: Partial<TextareaProps>;
+export const FormTextareaDefaultProps: Partial<TextareaProps> = {};
 
 export const FormInputDefaultProps = {
 };
