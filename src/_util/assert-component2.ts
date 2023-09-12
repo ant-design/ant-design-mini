576f81e0ba65ce99ddb487a3d89e98e67e071d86
DiamondYuan <fandi.yfd@antgroup.com>
2023-09-12 14:27:36 +0800

    docs: update document

--- src/_util/assert-component2.ts
+++ src/_util/assert-component2.ts
@@ -1,7 +1,12 @@
 function assertComponent2() {
   const component2 = my.canIUse('component2');
   if (!component2) {
-    throw new Error('需要使用component2');
+    console.log('监测到项目未开启 component2');
+    console.log(
+      '请在 IDE 中的 详情 > 项目配置 中，勾选 启用 component2 编译 。'
+    );
+    console.log('参考文档 https://mini.ant.design/guide/migration-v2');
+    throw new Error('需要使用component2。');
   }
 }
 
