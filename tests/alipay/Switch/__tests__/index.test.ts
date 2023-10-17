c06c49e5f29049630f9aefbc2945541e8b9206d5
DiamondYuan <541832074@qq.com>
2023-10-17 12:09:47 +0800

    fix: 修复 switch 的 value 不正确的问题 (#868)
    * fix: 修复 switch 逻辑错误
    
    * fix: 更新 switch 端的测试
    
    * fix: 新增 slider 的测试
    
    * fix: 更新用例
    
    ---------
    
    Co-authored-by: DiamondYuan <fandi.yfd@antgroup.com>

--- tests/alipay/Switch/__tests__/index.test.ts
+++ tests/alipay/Switch/__tests__/index.test.ts
@@ -15,6 +15,7 @@ it('受控模式', async () => {
   const onChange = vi.fn();
   const instance = testSwitch({
     checked: false,
+    'data-instance-uid': '1',
     onChange,
   });
 
@@ -24,6 +25,24 @@ it('受控模式', async () => {
   instance.callMethod('onChange', {});
   await sleep(20);
   expect(onChange.mock.calls.length).toBe(1);
+  expect(onChange.mock.calls[0][0]).toBe(true);
+  expect(onChange.mock.calls[0][1]).toMatchInlineSnapshot(`
+    {
+      "currentTarget": {
+        "dataset": {
+          "instance-uid": "1",
+        },
+      },
+      "target": {
+        "dataset": {
+          "instance-uid": "1",
+        },
+        "targetDataset": {
+          "instance-uid": "1",
+        },
+      },
+    }
+  `);
   expect(instance.getData()).toEqual({
     mixin: { value: false },
   });
@@ -67,4 +86,11 @@ it('测试默认值', async () => {
   expect(instance.getData()).toEqual({
     mixin: { value: false },
   });
+});
+
+it('测试构造函数', async () => {
+  const instance = testSwitch({});
+  expect(instance.getConfig()).toMatchFileSnapshot(
+    'snapshot/alipay_config.txt'
+  );
 });
