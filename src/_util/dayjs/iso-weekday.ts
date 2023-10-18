c5b7a605e02c24634fccb5a50d429d7e9363b8b4
DiamondYuan <fandi.yfd@antgroup.com>
2023-10-18 10:53:14 +0800

    fix: 优化文档

--- src/_util/dayjs/iso-weekday.ts
+++ src/_util/dayjs/iso-weekday.ts
@@ -1,5 +1,8 @@
 import { Dayjs } from 'dayjs';
 
-export function isoWeekday(da: Dayjs) {
-  return da.day() || 7;
+/**
+ * see https://github.com/iamkun/dayjs/blob/305f54099172ada45d10ba4c9bb5ec95e2a9d441/src/plugin/isoWeek/index.js#L37
+ */
+export function isoWeekday(day: Dayjs): number {
+  return day.day() || 7;
 }
