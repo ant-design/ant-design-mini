d4f741e78d48e0368c70d8bde8a8767607c8c145
DiamondYuan <fandi.yfd@antgroup.com>
2023-10-18 10:55:47 +0800

    fix: 构建代码

--- compiled/wechat/src/_util/dayjs/iso-weekday.js
+++ compiled/wechat/src/_util/dayjs/iso-weekday.js
@@ -1,3 +1,6 @@
-export function isoWeekday(da) {
-    return da.day() || 7;
+/**
+ * see https://github.com/iamkun/dayjs/blob/305f54099172ada45d10ba4c9bb5ec95e2a9d441/src/plugin/isoWeek/index.js#L37
+ */
+export function isoWeekday(day) {
+    return day.day() || 7;
 }
