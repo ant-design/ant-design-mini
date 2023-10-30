7d164b2c2465f1622e43b949e562d3082a7cd0a7
DiamondYuan <fandi.yfd@antgroup.com>
2023-10-30 19:04:48 +0800

    fix: 修复类型错误

--- src/Form/FormInput/props.ts
+++ src/Form/FormInput/props.ts
@@ -2,7 +2,7 @@ import { FormItemProps } from '../FormItem/props';
 import { InputProps } from '../../Input/props';
 
 
-export interface FormInputProps extends Omit<InputProps, 'value' | 'defaultValue'>, FormItemProps {
+export interface FormInputProps extends Omit<InputProps, 'value' | 'defaultValue'| 'name'>, FormItemProps {
   inputClassName: string;
   inputClassStyle: string;
 }
