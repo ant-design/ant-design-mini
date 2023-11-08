6b31241bbf3463bfd3553c3a2d9f3dd45ec756fb
DiamondYuan <fandi.yfd@antgroup.com>
2023-11-08 15:57:55 +0800

    feat: 更新 input blur 的测试

--- tests/alipay/Input/InputBlur/index.test.ts
+++ tests/alipay/Input/InputBlur/index.test.ts
@@ -1,29 +1,19 @@
 import { callMethod, getInstance, wrapValue } from 'tests/utils';
 import { describe, expect, it, vi } from 'vitest';
 
-describe('InputBlur 的 Controlled 模式', () => {
-  it('如果有 controlled, 以 controlled 为准', async () => {
-    const instance = getInstance('Input/InputBlur', {
-      value: '1',
-      controlled: false,
-    });
-    expect(instance.getData().mixin.value).toBe('1');
-    expect(instance.getData().mixin.controlled).toBe(false);
-  });
-
-  it('test onChange', async () => {
+describe('InputBlur 的受控模式', () => {
+  it('InputBlur 即使在受控模式下, 也会更新', async () => {
     const onChange = vi.fn();
     const instance = getInstance('Input/InputBlur', {
       value: '1',
       onChange,
     });
     expect(instance.getData().mixin.value).toBe('1');
-    expect(instance.getData().mixin.controlled).toBe(true);
     await callMethod(instance, 'onChange', wrapValue('3'));
     expect(instance.getData().mixin.value).toMatchInlineSnapshot('"3"');
   });
 
-  it('test onFocus', async () => {
+  it('onFocus 时，不会随着 value 改变而改变', async () => {
     const onFocus = vi.fn();
     const instance = getInstance('Input/InputBlur', {
       value: '1',
@@ -33,25 +23,33 @@ describe('InputBlur 的 Controlled 模式', () => {
     await callMethod(instance, 'onFocus', wrapValue('2'));
     expect(onFocus.mock.calls.map((o) => o[0])).toEqual(['2']);
     expect(instance.getData().mixin.value).toMatchInlineSnapshot('"1"');
+
+    // focus 的时候不响应 props 的变化
+    instance.setProps({ value: '2' });
+    expect(instance.getData().mixin.value).toEqual('1');
+
     await callMethod(instance, 'onChange', wrapValue('4'));
     expect(instance.getData().mixin.value).toMatchInlineSnapshot('"4"');
-
-    // focus 的时候不响应 value 的变化
-    instance.setProps({ value: '2' });
-    expect(instance.getData().mixin.value).toEqual('4');
   });
 
-  it('test onBlur', async () => {
+  it('onBlur 时候需要将 value 更新为 props 的数值', async () => {
     const onBlur = vi.fn();
     const instance = getInstance('Input/InputBlur', {
       value: '1',
       onBlur,
     });
+    await callMethod(instance, 'onFocus', wrapValue('2'));
+
     await callMethod(instance, 'onChange', wrapValue('2'));
     expect(instance.getData().mixin.value).toEqual('2');
+
+    instance.setProps({ value: '2.0' });
+    expect(instance.getData().mixin.value).toEqual('2');
+
+    // on blur 时候, 将 value 更新为 props.value
     await callMethod(instance, 'onBlur', wrapValue('4'));
-    expect(instance.getData().mixin.value).toMatchInlineSnapshot('"1"');
     expect(onBlur.mock.calls.map((o) => o[0])).toEqual(['4']);
+    expect(instance.getData().mixin.value).toEqual('2.0');
   });
 
   it('test onConfirm', async () => {
@@ -63,5 +61,34 @@ describe('InputBlur 的 Controlled 模式', () => {
     await callMethod(instance, 'onConfirm', wrapValue('2'));
     expect(instance.getData().mixin.value).toMatchInlineSnapshot('"1"');
     expect(onConfirm.mock.calls.map((o) => o[0])).toEqual(['2']);
+  });
+});
+
+describe('InputBlur 的非受控模式', () => {
+  it('非受控模式下, 内部的 value 一直不变, value 一直是 undefined', async () => {
+    const onChange = vi.fn();
+    const instance = getInstance('Input/InputBlur', {
+      onChange,
+    });
+    expect(instance.getData().mixin.value).toBe(undefined);
+    await callMethod(instance, 'onChange', wrapValue('3'));
+    expect(onChange.mock.calls.map((o) => o[0])).toEqual(['3']);
+    expect(instance.getData().mixin.value).toBe(undefined);
+  });
+
+  it('controlled 优先级高于 value', async () => {
+    const onChange = vi.fn();
+    const instance = getInstance('Input/InputBlur', {
+      value: '3',
+      controlled: false,
+      onChange,
+    });
+    expect(instance.getData().mixin.value).toBe('3');
+    await callMethod(instance, 'onChange', wrapValue('2'));
+    expect(onChange.mock.calls.map((o) => o[0])).toEqual(['2']);
+    expect(instance.getData().mixin.value).toBe('3');
+
+    instance.setProps({ value: '4' });
+    expect(instance.getData().mixin.value).toBe('4');
   });
 });
