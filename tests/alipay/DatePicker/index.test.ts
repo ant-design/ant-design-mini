555306adb4fb499a92824e778898429bfd35fe7f
DiamondYuan <fandi.yfd@antgroup.com>
2023-12-08 11:46:59 +0800

    feat: 更新 date picker 的测试

---
+++ tests/alipay/DatePicker/index.test.ts
@@ -0,0 +1,193 @@
+import dayjs from 'dayjs';
+import { describe, expect, it } from 'vitest';
+import { createDatePicker } from './utils';
+
+const date = [
+  dayjs().get('year'),
+  dayjs().get('month') + 1,
+  dayjs().get('date'),
+];
+
+describe('DatePicker', () => {
+  it('测试 columns', async () => {
+    const { instance } = createDatePicker();
+    instance.callMethod('onVisibleChange', true);
+    expect(instance.getData().currentValue).toEqual(date);
+    expect(instance.getData().columns.length).toEqual(3);
+    expect(instance.getData().columns[0]).toEqual(
+      new Array(21).fill(1).map((_, i) => {
+        const value = dayjs().subtract(10, 'year').add(i, 'year').get('year');
+        return {
+          label: `${value}年`,
+          value: value,
+        };
+      })
+    );
+    expect(instance.getData().columns[1]).toEqual(
+      new Array(12).fill(1).map((_, i) => {
+        return {
+          label: `${i + 1}月`,
+          value: i + 1,
+        };
+      })
+    );
+    expect(instance.getData().columns[2]).toEqual(
+      new Array(dayjs().endOf('month').date()).fill(1).map((_, i) => {
+        return {
+          label: `${i + 1}日`,
+          value: i + 1,
+        };
+      })
+    );
+  });
+
+  it('非受控模式修改', async () => {
+    const { instance, changeSelect, callOk } = createDatePicker({
+      defaultValue: dayjs('2023-01-01').toDate(),
+    });
+    instance.callMethod('onVisibleChange', true);
+    expect(instance.callMethod('onFormat')).toEqual('2023/01/01');
+    await changeSelect([2023, 2, 1]);
+    await callOk();
+    expect(instance.callMethod('onFormat')).toEqual('2023/02/01');
+  });
+
+  it('测试 onFormat', async () => {
+    let x = '';
+    const { instance } = createDatePicker({
+      defaultValue: dayjs('2023-01-01').toDate(),
+      format: 'YYYY / MM / DD',
+      onFormat: (v, datestr) => {
+        x = datestr;
+        return dayjs(v).format('YYYY--MM--DD');
+      },
+    });
+    expect(instance.callMethod('onFormat')).toEqual('2023--01--01');
+    expect(x).toEqual('2023 / 01 / 01');
+  });
+
+  it('测试 min max', async () => {
+    const { instance, changeSelect, callOk } = createDatePicker({
+      min: dayjs('2023-01-20').toDate(),
+      max: dayjs('2023-05-15').toDate(),
+      defaultValue: dayjs('2023-02-01').toDate(),
+    });
+    expect(instance.callMethod('onFormat')).toEqual('2023/02/01');
+    instance.callMethod('onVisibleChange', true);
+    expect(
+      instance.getData().columns.map((o) => {
+        return o.map((p) => `${p.label}`).join(',');
+      })
+    ).toMatchInlineSnapshot(
+      `
+      [
+        "2023年",
+        "1月,2月,3月,4月,5月",
+        "1日,2日,3日,4日,5日,6日,7日,8日,9日,10日,11日,12日,13日,14日,15日,16日,17日,18日,19日,20日,21日,22日,23日,24日,25日,26日,27日,28日",
+      ]
+    `
+    );
+    await changeSelect([2023, 1, 1]);
+    expect(
+      instance.getData().columns.map((o) => {
+        return o.map((p) => `${p.label}`).join(',');
+      })
+    ).toMatchInlineSnapshot(
+      `
+      [
+        "2023年",
+        "1月,2月,3月,4月,5月",
+        "20日,21日,22日,23日,24日,25日,26日,27日,28日,29日,30日,31日",
+      ]
+    `
+    );
+    expect(instance.getData().currentValue).toEqual([2023, 1, 20]);
+
+    await changeSelect([2023, 5, 30]);
+    expect(instance.getData().currentValue).toEqual([2023, 5, 15]);
+
+    await changeSelect([2023, 5, 12]);
+    expect(instance.getData().currentValue).toEqual([2023, 5, 12]);
+
+    await callOk();
+    expect(instance.callMethod('onFormat')).toEqual('2023/05/12');
+  });
+});
+
+describe('受控模式', () => {
+  it('测试', async () => {
+    const { instance, changeSelect, callOk, onOk } = createDatePicker({
+      value: dayjs('2023-01-01').toDate(),
+      defaultValue: dayjs('2023-01-02').toDate(),
+    });
+    instance.callMethod('onVisibleChange', true);
+    expect(instance.callMethod('onFormat')).toEqual('2023/01/01');
+    await changeSelect([2023, 2, 1]);
+    expect(instance.getData().currentValue).toEqual([2023, 2, 1]);
+    await callOk();
+    // 数据应该不变
+    expect(instance.callMethod('onFormat')).toEqual('2023/01/01');
+    expect(dayjs(onOk.mock.calls[0][0]).format('YYYY-MM-DD')).toEqual(
+      '2023-02-01'
+    );
+    expect(onOk.mock.calls[0][1]).toEqual('2023/02/01');
+  });
+});
+
+describe('各个精度', () => {
+  function getColumnText(precision: string) {
+    const { instance } = createDatePicker({
+      defaultValue: dayjs('2023-01-01').toDate(),
+      min: dayjs('2023-01-20').toDate(),
+      max: dayjs('2024-12-10').toDate(),
+      precision: precision as any,
+    });
+    instance.callMethod('onVisibleChange', true);
+    return instance
+      .getData()
+      .columns.map((o) => {
+        return o.map((p) => `${p.label}`).join(',');
+      })
+      .join('\n');
+  }
+
+  it('精度为年', async () => {
+    expect(getColumnText('year')).toEqual('2023年,2024年');
+  });
+
+  it('精度为月', async () => {
+    expect(getColumnText('month')).toEqual(
+      `2023年,2024年\n1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月`
+    );
+  });
+
+  it('精度为小时', async () => {
+    expect(getColumnText('hour')).toEqual(
+      `2023年,2024年
+1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月
+20日,21日,22日,23日,24日,25日,26日,27日,28日,29日,30日,31日
+0时,1时,2时,3时,4时,5时,6时,7时,8时,9时,10时,11时,12时,13时,14时,15时,16时,17时,18时,19时,20时,21时,22时,23时`
+    );
+  });
+
+  it('精度为分', async () => {
+    expect(getColumnText('minute')).toEqual(
+      `2023年,2024年
+1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月
+20日,21日,22日,23日,24日,25日,26日,27日,28日,29日,30日,31日
+0时,1时,2时,3时,4时,5时,6时,7时,8时,9时,10时,11时,12时,13时,14时,15时,16时,17时,18时,19时,20时,21时,22时,23时
+0分,1分,2分,3分,4分,5分,6分,7分,8分,9分,10分,11分,12分,13分,14分,15分,16分,17分,18分,19分,20分,21分,22分,23分,24分,25分,26分,27分,28分,29分,30分,31分,32分,33分,34分,35分,36分,37分,38分,39分,40分,41分,42分,43分,44分,45分,46分,47分,48分,49分,50分,51分,52分,53分,54分,55分,56分,57分,58分,59分`
+    );
+  });
+
+  it('精度为秒', async () => {
+    expect(getColumnText('second')).toEqual(
+      `2023年,2024年
+1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月
+20日,21日,22日,23日,24日,25日,26日,27日,28日,29日,30日,31日
+0时,1时,2时,3时,4时,5时,6时,7时,8时,9时,10时,11时,12时,13时,14时,15时,16时,17时,18时,19时,20时,21时,22时,23时
+0分,1分,2分,3分,4分,5分,6分,7分,8分,9分,10分,11分,12分,13分,14分,15分,16分,17分,18分,19分,20分,21分,22分,23分,24分,25分,26分,27分,28分,29分,30分,31分,32分,33分,34分,35分,36分,37分,38分,39分,40分,41分,42分,43分,44分,45分,46分,47分,48分,49分,50分,51分,52分,53分,54分,55分,56分,57分,58分,59分
+0秒,1秒,2秒,3秒,4秒,5秒,6秒,7秒,8秒,9秒,10秒,11秒,12秒,13秒,14秒,15秒,16秒,17秒,18秒,19秒,20秒,21秒,22秒,23秒,24秒,25秒,26秒,27秒,28秒,29秒,30秒,31秒,32秒,33秒,34秒,35秒,36秒,37秒,38秒,39秒,40秒,41秒,42秒,43秒,44秒,45秒,46秒,47秒,48秒,49秒,50秒,51秒,52秒,53秒,54秒,55秒,56秒,57秒,58秒,59秒`
+    );
+  });
+});
---
+++ tests/alipay/DatePicker/utils.ts
@@ -0,0 +1,38 @@
+import { IDatePickerProps } from 'compiled-alipay/DatePicker/props';
+import { getInstance, sleep } from 'tests/utils';
+import { vi } from 'vitest';
+
+export const createDatePicker = (props?: Partial<IDatePickerProps>) => {
+  const onOk = vi.fn();
+  const onCancel = vi.fn();
+  const onPickerChange = vi.fn();
+  const onVisibleChange = vi.fn();
+
+  const instance = getInstance('DatePicker', {
+    onOk,
+    onCancel,
+    onPickerChange,
+    onVisibleChange,
+    ...(props || {}),
+  });
+
+  async function changeSelect(value) {
+    instance.callMethod('onChange', value);
+    await sleep(20);
+  }
+
+  async function callOk() {
+    instance.callMethod('onOk');
+    await sleep(20);
+  }
+
+  return {
+    changeSelect,
+    callOk,
+    onOk,
+    onCancel,
+    onPickerChange,
+    onVisibleChange,
+    instance,
+  };
+};
