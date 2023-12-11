c3431f3d0d6a6253af963a72f58ef1766f984142
DiamondYuan <fandi.yfd@antgroup.com>
2023-12-11 11:35:27 +0800

    feat: 更新 selector 的测试

--- tests/alipay/Selector/index.test.ts
+++ tests/alipay/Selector/index.test.ts
@@ -1,3 +1,4 @@
+import fmtEvent from 'compiled-alipay/_util/fmtEvent';
 import { describe, expect, it } from 'vitest';
 import { createSelector } from './utils';
 
@@ -73,5 +74,107 @@ describe('Selector Test', () => {
       await selectByValue(1);
       expect(instance.getData().mixin.value).toEqual(1);
     });
+
+    it('测试 disabled', async () => {
+      const { instance, selectByValue } = createSelector({
+        defaultValue: 1,
+        disabled: true,
+      });
+      expect(instance.getData().mixin.value).toEqual(1);
+      await selectByValue(2);
+      expect(instance.getData().mixin.value).toEqual(1);
+    });
+  });
+
+  it('onChange 事件 单选', async () => {
+    const { instance, selectByValue, onChange } = createSelector({
+      defaultValue: 1,
+    });
+    expect(instance.getData().mixin.value).toEqual(1);
+    await selectByValue(2);
+    expect(onChange.mock.lastCall).toEqual([
+      2,
+      { text: '2', value: 2 },
+      fmtEvent({}),
+    ]);
+    expect(instance.getData().mixin.value).toEqual(2);
+    await selectByValue(2);
+    expect(onChange.mock.lastCall).toEqual([
+      undefined,
+      undefined,
+      fmtEvent({}),
+    ]);
+    expect(instance.getData().mixin.value).toEqual(undefined);
+  });
+
+  it('onChange 事件 多选', async () => {
+    const { instance, selectByValue, onChange } = createSelector({
+      multiple: true,
+      defaultValue: [1, 2],
+    });
+    expect(instance.getData().mixin.value).toEqual([1, 2]);
+    await selectByValue(2);
+    expect(onChange.mock.lastCall).toEqual([
+      [1],
+      [{ text: '1', value: 1 }],
+      fmtEvent({}),
+    ]);
+    expect(instance.getData().mixin.value).toEqual([1]);
+    await selectByValue(3);
+    expect(onChange.mock.lastCall).toEqual([
+      [1, 3],
+      [
+        { text: '1', value: 1 },
+        { text: '3', value: 3 },
+      ],
+      fmtEvent({}),
+    ]);
+  });
+});
+
+describe('受控模式', () => {
+  it('单选', async () => {
+    const { instance, selectByValue, onChange } = createSelector({
+      value: 1,
+    });
+    expect(instance.getData().mixin.value).toEqual(1);
+    await selectByValue(2);
+    expect(onChange.mock.lastCall).toEqual([
+      2,
+      { text: '2', value: 2 },
+      fmtEvent({}),
+    ]);
+    expect(instance.getData().mixin.value).toEqual(1);
+    await selectByValue(1);
+    expect(onChange.mock.lastCall).toEqual([
+      undefined,
+      undefined,
+      fmtEvent({}),
+    ]);
+  });
+
+  it('多选', async () => {
+    const { instance, selectByValue, onChange } = createSelector({
+      multiple: true,
+      value: [1, 2],
+    });
+    expect(instance.getData().mixin.value).toEqual([1, 2]);
+    await selectByValue(2);
+    expect(onChange.mock.lastCall).toEqual([
+      [1],
+      [{ text: '1', value: 1 }],
+      fmtEvent({}),
+    ]);
+    expect(instance.getData().mixin.value).toEqual([1, 2]);
+    await selectByValue(3);
+    expect(onChange.mock.lastCall).toEqual([
+      [1, 2, 3],
+      [
+        { text: '1', value: 1 },
+        { text: '2', value: 2 },
+        { text: '3', value: 3 },
+      ],
+      fmtEvent({}),
+    ]);
   });
 });
