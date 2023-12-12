64db391b283721f04bb8dd5042600e007027f87c
DiamondYuan <fandi.yfd@antgroup.com>
2023-12-12 15:34:00 +0800

    feat: 使用 tsx 重写 datepicker 的 axml

--- compiled/alipay/demo/pages/DatePicker/index.js
+++ compiled/alipay/demo/pages/DatePicker/index.js
@@ -1,33 +1,32 @@
 Page({
-  data: {
-    min: new Date('2019/01/15'),
-    max: new Date('2023/08/20'),
-    defaultDate: new Date('2019/02/02'),
-    defaultDateRange: [new Date('2022/03/21'), new Date('2022/05/20')],
-  },
-  handlePickerChange(date, dateStr, e) {
-    console.log('onPickerChange', date, dateStr, e);
-  },
-  handleOk(date, format, e) {
-    console.log('onOk', date, format, e);
-  },
-  handlePickerRangeChange(type, date, dateStr, e) {
-    console.log('onPickerRangeChange', type, date, dateStr, e);
-  },
-  handleRangeOk(date, format, e) {
-    console.log('onRangeOk', date, format, e);
-  },
-  handleChangeDate() {
-    this.setData({ defaultDate: new Date('2019/05/02') });
-  },
-  handleTriggerPicker(visible, e) {
-    console.log('onVisibleChange', visible, e);
-  },
-
-  handleDismiss(e) {
-    console.log('e', e);
-  },
-  handleFormatLabel(type, value) {
-    return String(value);
-  },
+    data: {
+        min: new Date('2019/01/15'),
+        max: new Date('2023/08/20'),
+        defaultDate: new Date('2019/02/02'),
+        defaultDateRange: [new Date('2022/03/21'), new Date('2022/05/20')],
+    },
+    handlePickerChange: function (date, dateStr, e) {
+        console.log('onPickerChange', date, dateStr, e);
+    },
+    handleOk: function (date, format, e) {
+        console.log('onOk', date, format, e);
+    },
+    handlePickerRangeChange: function (type, date, dateStr, e) {
+        console.log('onPickerRangeChange', type, date, dateStr, e);
+    },
+    handleRangeOk: function (date, format, e) {
+        console.log('onRangeOk', date, format, e);
+    },
+    handleChangeDate: function () {
+        this.setData({ defaultDate: new Date('2019/05/02') });
+    },
+    handleTriggerPicker: function (visible, e) {
+        console.log('onVisibleChange', visible, e);
+    },
+    handleDismiss: function (e) {
+        console.log('e', e);
+    },
+    handleFormatLabel: function (type, value) {
+        return String(value);
+    },
 });
---
+++ demo/pages/DatePicker/index.axml.tsx
@@ -0,0 +1,126 @@
+import { InternalData, Page } from 'tsxml';
+import AntList from '../../../src/List/index.axml';
+import AntListItem from '../../../src/List/ListItem/index.axml';
+import AntDatePicker from '../../../src/DatePicker/index.axml';
+import AntRangePicker from '../../../src/DatePicker/RangePicker/index.axml';
+
+export default ({ min, max, defaultDateRange, defaultDate }: InternalData) => (
+  <Page>
+    <AntList header="基础用法">
+      <AntListItem>
+        请选择日期
+        <AntDatePicker
+          min={min}
+          max={max}
+          slot="extra"
+          defaultValue={defaultDate}
+          placeholder="请选择"
+          onPickerChange="handlePickerChange"
+          onVisibleChange="handleTriggerPicker"
+          onCancel="handleDismiss"
+          onOk="handleOk"
+        ></AntDatePicker>
+      </AntListItem>
+      <AntListItem>
+        请选择日期（点击蒙层不关闭）
+        <AntDatePicker
+          min={min}
+          max={max}
+          slot="extra"
+          defaultValue={defaultDate}
+          placeholder="请选择"
+          onPickerChange="handlePickerChange"
+          onVisibleChange="handleTriggerPicker"
+          maskClosable={false}
+          onOk="handleOk"
+        ></AntDatePicker>
+      </AntListItem>
+    </AntList>
+    <AntList header="选择精度">
+      <AntListItem>
+        请选择年度
+        <AntDatePicker
+          slot="extra"
+          precision="year"
+          format="YYYY"
+          placeholder="选择"
+          onOk="handleOk"
+        />
+      </AntListItem>
+      <AntListItem>
+        请选择月份
+        <AntDatePicker
+          slot="extra"
+          precision="month"
+          format="YYYY-MM"
+          placeholder="选择"
+          onVisibleChange="handleTriggerPicker"
+          onOk="handleOk"
+        />
+      </AntListItem>
+      <AntListItem>
+        请选择时间
+        <AntDatePicker
+          slot="extra"
+          precision="minute"
+          format="YYYY-MM-DD HH:mm"
+          placeholder="选择"
+          onVisibleChange="handleTriggerPicker"
+          onOk="handleOk"
+        />
+      </AntListItem>
+    </AntList>
+    <AntList header="禁用状态">
+      <AntListItem>
+        请选择日期
+        <AntDatePicker
+          slot="extra"
+          defaultValue={defaultDate}
+          disabled
+        ></AntDatePicker>
+      </AntListItem>
+    </AntList>
+    <AntList header="自定义每列的渲染内容">
+      <AntListItem>
+        每列不显示单位
+        <AntDatePicker
+          min={min}
+          max={max}
+          slot="extra"
+          defaultValue={defaultDate}
+          placeholder="请选择"
+          onPickerChange="handlePickerChange"
+          onVisibleChange="handleTriggerPicker"
+          onCancel="handleDismiss"
+          onOk="handleOk"
+          onFormatLabel="handleFormatLabel"
+        ></AntDatePicker>
+      </AntListItem>
+    </AntList>
+    <AntList header="时间范围选择">
+      <AntListItem>
+        日期范围
+        <AntRangePicker
+          slot="extra"
+          placeholder="请选择"
+          defaultValue={defaultDateRange}
+          onPickerChange="handlePickerRangeChange"
+          onVisibleChange="handleTriggerPicker"
+          onOk="handleRangeOk"
+        ></AntRangePicker>
+      </AntListItem>
+      <AntListItem>
+        每列不显示单位
+        <AntRangePicker
+          slot="extra"
+          placeholder="请选择"
+          defaultValue={defaultDateRange}
+          onPickerChange="handlePickerRangeChange"
+          onVisibleChange="handleTriggerPicker"
+          onOk="handleRangeOk"
+          onFormatLabel="handleFormatLabel"
+        ></AntRangePicker>
+      </AntListItem>
+    </AntList>
+  </Page>
+);
