5a8f19e6cd2042be8e3ddd0cde4c09a567887549
DiamondYuan <fandi.yfd@antgroup.com>
2023-12-15 16:31:58 +0800

    feat: 重写 selector

--- src/Selector/index.ts
+++ src/Selector/index.ts
@@ -1,101 +1,106 @@
-import { SelectorDefaultProps, ISelectorItem } from './props';
-import mixinValue from '../mixins/value';
-import fmtEvent from '../_util/fmtEvent';
+import { useEvent } from 'functional-mini/component';
 import '../_util/assert-component2';
+import { mountComponent } from '../_util/component';
+import { useComponentEvent } from '../_util/hooks/useComponentEvent';
+import { useMixState } from '../_util/hooks/useMixState';
+import { ISelectorProps, Value } from './props';
 
-Component({
-  mixins: [mixinValue()],
-  props: SelectorDefaultProps,
-  methods: {
-    onChange(e) {
-      const event = fmtEvent(this.props);
-      const { disabled, value } = e.currentTarget.dataset;
-      const {
-        multiple,
-        options,
-        maxSelectedCount,
-        minSelectedCount,
-        onSelectMax,
-        onSelectMin,
-        onChange,
-      } = this.props;
-      if (disabled || this.props.disabled) {
-        return;
-      }
-      if (multiple) {
-        let currentValue = this.getValue() || [];
-        if (currentValue.indexOf(value) > -1) {
-          if (
-            !isNaN(minSelectedCount) &&
-            currentValue.length <= minSelectedCount
-          ) {
-            if (onSelectMin) {
-              onSelectMin(
-                value,
-                options.find((v) => v.value === value) as ISelectorItem,
-                event
-              );
-            }
-            return;
-          }
-          currentValue = currentValue.filter((v) => v !== value);
-        } else {
-          if (
-            !isNaN(maxSelectedCount) &&
-            currentValue.length >= maxSelectedCount
-          ) {
-            if (onSelectMax) {
-              onSelectMax(
-                value,
-                options.find((v) => v.value === value) as ISelectorItem,
-                event
-              );
-            }
-            return;
-          }
-          currentValue = [...currentValue, value];
-        }
-        if (!this.isControlled()) {
-          this.update(currentValue);
-        }
-        if (onChange) {
-          onChange(
-            currentValue,
-            options.filter((v) => currentValue.indexOf(v.value) > -1),
-            fmtEvent(this.props)
+const Selector = (props: ISelectorProps) => {
+  const [selectorValue, { isControlled, update }] = useMixState(
+    props.defaultValue,
+    {
+      value: props.value,
+    }
+  );
+  const { triggerEventValues } = useComponentEvent(props);
+  useEvent('onChange', (e) => {
+    const { disabled, value } = e.currentTarget.dataset;
+    const { multiple, options, maxSelectedCount, minSelectedCount } = props;
+    if (disabled || props.disabled) {
+      return;
+    }
+    if (multiple) {
+      let currentValue = (selectorValue as Value[]) || [];
+      if (currentValue.indexOf(value) > -1) {
+        if (
+          !isNaN(minSelectedCount) &&
+          currentValue.length <= minSelectedCount
+        ) {
+          triggerEventValues(
+            'selectMin',
+            [value, options.find((v) => v.value === value)],
+            e
           );
+          return;
         }
+        currentValue = currentValue.filter((v) => v !== value);
       } else {
-        if (value === this.getValue()) {
-          if (minSelectedCount === 1) {
-            if (onSelectMin) {
-              onSelectMin(
-                value,
-                options.find((v) => v.value === value),
-                event
-              );
-            }
-            return;
-          }
-          if (!this.isControlled()) {
-            this.update(undefined);
-          }
-          if (onChange) {
-            onChange(undefined, undefined, fmtEvent(this.props));
-          }
-        } else {
-          if (!this.isControlled()) {
-            this.update(value);
-          }
-          if (onChange) {
-            onChange(
-              value,
-              options.find((v) => v.value === value),
-              fmtEvent(this.props)
-            );
-          }
+        if (
+          !isNaN(maxSelectedCount) &&
+          currentValue.length >= maxSelectedCount
+        ) {
+          triggerEventValues(
+            'onSelectMax',
+            [value, options.find((v) => v.value === value)],
+            e
+          );
+          return;
         }
+        currentValue = [...currentValue, value];
       }
+      if (!isControlled) {
+        update(currentValue);
+      }
+      triggerEventValues(
+        'change',
+        [
+          currentValue,
+          options.filter((v) => currentValue.indexOf(v.value) > -1),
+        ],
+        e
+      );
+    } else {
+      if (value === selectorValue) {
+        if (minSelectedCount === 1) {
+          triggerEventValues(
+            'selectMin',
+            [value, options.find((v) => v.value === value)],
+            e
+          );
+          return;
+        }
+        if (!isControlled) {
+          update(undefined);
+        }
+        triggerEventValues('change', [undefined, undefined], e);
+      } else {
+        if (!isControlled) {
+          update(value);
+        }
+        triggerEventValues(
+          'change',
+          [value, options.find((v) => v.value === value)],
+          e
+        );
+      }
+    }
+  });
+
+  return {
+    mixin: {
+      value: selectorValue,
     },
-  },
+  };
+};
+
+mountComponent(Selector, {
+  value: null,
+  defaultValue: null,
+  options: null,
+  activeItemClassName: '',
+  activeItemStyle: '',
+  multiple: false,
+  minSelectedCount: null,
+  maxSelectedCount: null,
+  disabled: false,
 });
