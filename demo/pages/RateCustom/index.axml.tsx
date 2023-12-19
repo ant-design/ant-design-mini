d68be0f4cffcf0b9e830ef8d64099cc814a5c570
DiamondYuan <fandi.yfd@antgroup.com>
2023-12-19 14:21:16 +0800

    fix: 重命名标签

--- demo/pages/RateCustom/index.axml.tsx
+++ demo/pages/RateCustom/index.axml.tsx
@@ -1,65 +1,65 @@
 import { InternalData, View } from 'tsxml';
-import Container from '../../../src/Container/index.axml';
-import Icon from '../../../src/Icon/index.axml';
-import Rate from '../../../src/Rate/index.axml';
+import AntContainer from '../../../src/Container/index.axml';
+import AntIcon from '../../../src/Icon/index.axml';
+import AntRate from '../../../src/Rate/index.axml';
 
 export default ({ rate }: InternalData) => (
   <Component>
-    <Container title="自定义间距">
-      <Rate defaultValue={3} gutter={8} />
+    <AntContainer title="自定义间距">
+      <AntRate defaultValue={3} gutter={8} />
       <View class="description">8px</View>
 
-      <Rate defaultValue={4} gutter={16} />
+      <AntRate defaultValue={4} gutter={16} />
       <View class="description">16px</View>
-    </Container>
+    </AntContainer>
 
-    <Container title="自定义大小">
-      <Rate defaultValue={3} className="rate" />
+    <AntContainer title="自定义大小">
+      <AntRate defaultValue={3} className="rate" />
       <View class="description">30px</View>
 
-      <Rate defaultValue={3} style="font-size: 40px" />
+      <AntRate defaultValue={3} style="font-size: 40px" />
       <View class="description">40px</View>
-    </Container>
+    </AntContainer>
 
-    <Container title="自定义颜色">
-      <Rate
+    <AntContainer title="自定义颜色">
+      <AntRate
         defaultValue={3}
         characterClassName="star"
         characterActiveClassName="star-active"
       />
       <View class="description">基础</View>
 
-      <Rate
+      <AntRate
         defaultValue={3}
         characterClassName="star-gradient"
         characterActiveClassName="star-active-gradient"
       />
       <View class="description">渐变</View>
-    </Container>
+    </AntContainer>
 
     {/* #if WECHAT */}
-    <Container title="自定义图标">
-      <Rate defaultValue={3}>
-        <Icon slot="character" type="HeartFill" />
-      </Rate>
+    <AntContainer title="自定义图标">
+      <AntRate defaultValue={3}>
+        <AntIcon slot="character" type="HeartFill" />
+      </AntRate>
       <View class="description">自定义为HeartFill</View>
 
-      <Rate defaultValue={3.5} allowHalf>
-        <Icon slot="character" type="StarOutline" />
-      </Rate>
+      <AntRate defaultValue={3.5} allowHalf>
+        <AntIcon slot="character" type="StarOutline" />
+      </AntRate>
       <View class="description">自定义为StarOutline</View>
 
-      <Rate defaultValue={3.5} allowHalf characterClassName="character">
+      <AntRate defaultValue={3.5} allowHalf characterClassName="character">
         <View slot="character">正</View>
-      </Rate>
+      </AntRate>
       <View class="description">自定义为正</View>
 
-      <Rate defaultValue={2.5} allowHalf characterClassName="character">
+      <AntRate defaultValue={2.5} allowHalf characterClassName="character">
         <View slot="character">A </View>
-      </Rate>
+      </AntRate>
       <View class="description">自定义为A</View>
 
-      <Rate
+      <AntRate
         defaultValue={2.5}
         allowHalf
         gutter={20}
@@ -68,9 +68,9 @@ export default ({ rate }: InternalData) => (
         <View slot="character" slot-scope="rate">
           {rate.index}
         </View>
-      </Rate>
+      </AntRate>
       <View class="description">自定义为索引</View>
-    </Container>
+    </AntContainer>
     {/* #endif */}
   </Component>
 );
