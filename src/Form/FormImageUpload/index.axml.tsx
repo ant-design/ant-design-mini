62541fabcbc1af965a853dbfb43f5f65ec2a22e4
DiamondYuan <fandi.yfd@antgroup.com>
2023-12-21 11:52:54 +0800

    feat: 部分组件使用 tsx 重写

---
+++ src/Form/FormImageUpload/index.axml.tsx
@@ -0,0 +1,65 @@
+import { TSXMLProps, Component, View, Slot, InternalData } from 'tsxml';
+import FormItem from '../FormItem/index.axml';
+import ImageUpload from '../../ImageUpload/index.axml';
+import { FormImageUploadProps } from './props'; // Assuming the TSX file lives in the same directory as the 'props' file.
+
+export default (
+  {
+    className,
+    position,
+    label,
+    labelWidth,
+    tooltip,
+    requiredMark,
+    validateStatus,
+    help,
+    maxCount,
+    imageMode,
+    sourceType,
+  }: TSXMLProps<FormImageUploadProps>,
+  { formData, item }: InternalData
+) => (
+  <Component>
+    <FormItem
+      className={className}
+      position={position}
+      label={label}
+      labelWidth={labelWidth}
+      status={formData.status}
+      errors={formData.errors}
+      tooltip={tooltip}
+      required={formData.required}
+      requiredMark={requiredMark}
+      validateStatus={validateStatus}
+      help={help}
+    >
+      <ImageUpload
+        fileList={formData.value}
+        maxCount={maxCount}
+        imageMode={imageMode}
+        sourceType={sourceType}
+        onChange="onChange"
+        onUpload="onUpload"
+        onRemove="onRemove"
+        onPreview="onPreview"
+        onBeforeUpload="onBeforeUpload"
+        onChooseImageError="onChooseImageError"
+      />
+      <View slot="extra">
+        {/* #if ALIPAY */}
+        <Slot name="extra">
+          {/* #endif */}
+          {formData.extra}
+          {/* #if ALIPAY */}
+        </Slot>
+        {/* #endif */}
+      </View>
+      <View slot="header" slot-scope="item">
+        <Slot name="header" errors={item.errors} status={item.status} />
+      </View>
+      <View slot="footer" slot-scope="item">
+        <Slot name="footer" errors={item.errors} status={item.status} />
+      </View>
+    </FormItem>
+  </Component>
+);
---
+++ src/Form/FormInput/index.axml.tsx
@@ -0,0 +1,96 @@
+import { Component, Slot, TSXMLProps, InternalData, View } from 'tsxml';
+import FormItem from '../FormItem/index.axml';
+import AntInput from '../../Input/index.axml';
+import { FormInputProps } from './props'; // assuming the props file is in the same directory
+
+export default (
+  {
+    className,
+    position,
+    label,
+    labelWidth,
+    tooltip,
+    requiredMark,
+    validateStatus,
+    help,
+    inputClassName,
+    style: inputStyle,
+    disabled,
+    type,
+    password,
+    placeholder,
+    alwaysSystem,
+    placeholderClassName,
+    placeholderStyle,
+    maxLength,
+    confirmType,
+    confirmHold,
+    cursor,
+    selectionStart,
+    selectionEnd,
+    randomNumber,
+    allowClear,
+    enableNative,
+  }: TSXMLProps<FormInputProps>,
+  { extra, formData, item }: InternalData
+) => (
+  <Component>
+    <FormItem
+      className={className}
+      position={position}
+      label={label}
+      labelWidth={labelWidth}
+      status={formData.status}
+      errors={formData.errors}
+      tooltip={tooltip}
+      required={formData.required}
+      requiredMark={requiredMark}
+      validateStatus={validateStatus}
+      help={help}
+    >
+      <AntInput
+        ref="handleRef"
+        allowClear={allowClear}
+        enableNative={enableNative}
+        className={inputClassName}
+        style={inputStyle}
+        disabled={disabled}
+        type={type}
+        password={password}
+        placeholder={placeholder}
+        alwaysSystem={alwaysSystem}
+        placeholderClassName={placeholderClassName}
+        placeholderStyle={placeholderStyle}
+        maxLength={maxLength}
+        confirmType={confirmType}
+        confirmHold={confirmHold}
+        cursor={cursor}
+        selectionStart={selectionStart}
+        selectionEnd={selectionEnd}
+        randomNumber={randomNumber}
+        onChange="onChange"
+        onBlur="onBlur"
+        onFocus="onFocus"
+        onConfirm="onConfirm"
+      >
+        <Slot slot="prefix" name="prefix" />
+        <Slot slot="suffix" name="suffix" />
+      </AntInput>
+      <View slot="extra">
+        {/* #if ALIPAY */}
+        <Slot name="extra">
+          {/* #endif */}
+          {extra}
+          {/* #if ALIPAY */}
+        </Slot>
+        {/* #endif */}
+      </View>
+      <View slot="header" slot-scope="item">
+        <Slot name="header" errors={item.errors} status={item.status} />
+      </View>
+      <View slot="footer" slot-scope="item">
+        <Slot name="footer" errors={item.errors} status={item.status} />
+      </View>
+    </FormItem>
+  </Component>
+);
