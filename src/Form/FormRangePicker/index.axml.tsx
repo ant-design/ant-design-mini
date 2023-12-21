c2c0fb1bdd15098e6412dfc4d2720af32a78dc8d
DiamondYuan <fandi.yfd@antgroup.com>
2023-12-21 11:25:15 +0800

    feat: 更新 axml

---
+++ src/Form/FormRangePicker/index.axml.tsx
@@ -0,0 +1,115 @@
+import { View, Slot, TSXMLProps, InternalData } from 'tsxml';
+import FormItem from '../FormItem/index.axml';
+import RangePicker from '../../DatePicker/RangePicker/index.axml';
+import Icon from '../../Icon/index.axml';
+import { FormRangePickerProps } from './props';
+import arrowUtil from '../../_util/arrow.sjs';
+
+export default (
+  {
+    className,
+    position,
+    label,
+    labelWidth,
+    tooltip,
+    requiredMark,
+    style,
+    endPlaceholder,
+    startPlaceholder,
+    splitCharacter,
+    min,
+    max,
+    format,
+    precision,
+    animationType,
+    cancelText,
+    okText,
+    placeholder,
+    maskClosable,
+    maskStyle,
+    popClassName,
+    popStyle,
+    disabled,
+    title,
+    maskClassName,
+    indicatorStyle,
+    indicatorClassName,
+    extra,
+    arrow,
+  }: TSXMLProps<FormRangePickerProps>,
+  { formData, item }: InternalData
+) => (
+  <Component>
+    <FormItem
+      className={className}
+      position={position}
+      label={label}
+      labelWidth={labelWidth}
+      status={formData.status}
+      errors={formData.errors}
+      tooltip={tooltip}
+      required={formData.required}
+      requiredMark={requiredMark}
+      validateStatus={formData.validateStatus}
+      help={formData.help}
+      style={style}
+    >
+      <View>
+        <RangePicker
+          endPlaceholder={endPlaceholder}
+          startPlaceholder={startPlaceholder}
+          splitCharacter={splitCharacter}
+          min={min}
+          max={max}
+          format={format}
+          precision={precision}
+          animationType={animationType}
+          cancelText={cancelText}
+          okText={okText}
+          placeholder={placeholder}
+          maskClosable={maskClosable}
+          maskStyle={maskStyle}
+          popClassName={popClassName}
+          popStyle={popStyle}
+          disabled={disabled}
+          title={title}
+          maskClassName={maskClassName}
+          indicatorStyle={indicatorStyle}
+          indicatorClassName={indicatorClassName}
+          value={formData.value}
+          onOk="onOk"
+          onPickerChange="onPickerChange"
+          onCancel="onDismissPicker"
+          onVisibleChange="onVisibleChange"
+          onFormatLabel="onFormatLabel"
+          onFormat="onFormat"
+        >
+          <Slot name="content" slot="content" />
+          <Slot name="title" slot="title" />
+          {arrowUtil.getArrow(arrow) && (
+            <Icon
+              className="ant-form-range-picker-arrow"
+              type={arrowUtil.getArrow(arrow)}
+              slot="suffix"
+            />
+          )}
+        </RangePicker>
+      </View>
+      <View slot="extra">
+        {/* #if ALIPAY */}
+        <Slot name="extra">
+          {/* #endif */}
+          {extra}
+          {/* #if ALIPAY */}
+        </Slot>
+        {/* #endif */}
+      </View>
+      <View slot="header" slot-scope="item">
+        <Slot name="header" errors={item.errors} status={item.status} />
+      </View>
+      <View slot="footer" slot-scope="item">
+        <Slot name="footer" errors={item.errors} status={item.status} />
+      </View>
+    </FormItem>
+  </Component>
+);
