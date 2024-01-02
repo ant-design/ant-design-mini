903f2eb925c1974873e0f5efe26459d582b9ea25
DiamondYuan <fandi.yfd@antgroup.com>
2024-01-02 15:58:42 +0800

    fix: 修改产物为 es2019

--- compiled/alipay/demo/pages/Form/index.js
+++ compiled/alipay/demo/pages/Form/index.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import { Form } from '../../../src/Form/form';
 import cityList from './city';
 Page({
@@ -35,15 +26,13 @@ Page({
     reset() {
         this.form.reset();
     },
-    submit() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const values = yield this.form.submit();
-            my.alert({
-                title: '提交',
-                content: JSON.stringify(values, null, 2),
-            });
-            console.log(values);
+    async submit() {
+        const values = await this.form.submit();
+        my.alert({
+            title: '提交',
+            content: JSON.stringify(values, null, 2),
         });
+        console.log(values);
     },
     onUpload(localFile) {
         return new Promise((resolve) => {
--- compiled/alipay/demo/pages/FormCustom/index.js
+++ compiled/alipay/demo/pages/FormCustom/index.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import { Form } from '../../../src/Form/form';
 Page({
     data: {
@@ -26,13 +17,11 @@ Page({
     reset() {
         this.form.reset();
     },
-    submit() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const values = yield this.form.submit();
-            my.alert({
-                title: '提交',
-                content: JSON.stringify(values),
-            });
+    async submit() {
+        const values = await this.form.submit();
+        my.alert({
+            title: '提交',
+            content: JSON.stringify(values),
         });
     }
 });
