16109a7c478b4072c45286b6ed82b799326f0c04
DiamondYuan <fandi.yfd@antgroup.com>
2024-01-23 13:59:53 +0800

    wip

--- scripts/todo.ts
+++ scripts/todo.ts
@@ -1,15 +1,25 @@
 import * as fs from 'fs';
 
 async function check() {
-  const files = await fs.readdirSync('src');
+  const filesList = await fs.readdirSync('src');
+  const pagesList = await fs.readdirSync('demo/pages');
 
-  const { src } = JSON.parse(
+  const { src, pages } = JSON.parse(
     await fs.readFileSync('config/wechat.json', 'utf-8')
   );
 
-  const doneSet = new Set(['.umi-production', '.umi', 'tsxml'].concat(src));
+  const doneSet = new Set(
+    ['.umi-production', '.umi', 'tsxml'].concat(src).concat(pages)
+  );
 
-  files.forEach((e) => {
+  console.log('------files-------');
+  filesList.forEach((e) => {
+    if (!doneSet.has(e)) {
+      console.log(e);
+    }
+  });
+  console.log('------pages-------');
+  pagesList.forEach((e) => {
     if (!doneSet.has(e)) {
       console.log(e);
     }
