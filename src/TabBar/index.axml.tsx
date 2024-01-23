165e8d37c89f6099ed331b6ede5357562a6d4480
DiamondYuan <541832074@qq.com>
2024-01-23 11:42:56 +0800

    更新 tabbar 的 axml (#1051)
    * feat: 更新 tabbar 的 axml
    
    * feat: 更新 tabbar 的 axml
    
    * fix: 更新 tabbar 的 axml
    
    ---------
    
    Co-authored-by: DiamondYuan <fandi.yfd@antgroup.com>

---
+++ src/TabBar/index.axml.tsx
@@ -0,0 +1,87 @@
+import { Component, InternalData, Slot, Text, TSXMLProps, View } from 'tsxml';
+import AntBadge from '../Badge/index.axml';
+import ImageIcon from '../ImageIcon/index.axml';
+import { ITabBarProps } from './props';
+
+export default (
+  {
+    className,
+    style,
+    items,
+    activeClassName,
+    activeStyle,
+  }: TSXMLProps<ITabBarProps>,
+  { mixin }: InternalData
+) => (
+  <Component>
+    <View class={`ant-tab-bar ${className || ''}`} style={style || ''}>
+      <View class="ant-tab-bar-wrap">
+        {items.map((item, index) => (
+          <View
+            class={`ant-tab-bar-item ${
+              index === mixin.value
+                ? 'ant-tab-bar-item-active ' + (activeClassName || '')
+                : ''
+            }`}
+            style={index === mixin.value ? activeStyle || '' : ''}
+            onTap="onChange"
+            data-index={index}
+          >
+            {item.badge ? (
+              <AntBadge
+                type={item.badge.type}
+                text={item.badge.text}
+                number={item.badge.number}
+                stroke={item.badge.stroke}
+                bgColor={item.badge.bgColor}
+                position={item.badge.position || 'top-right'}
+                offsetX={item.badge.offsetX || '-9px'}
+                offsetY={item.badge.offsetY || '0px'}
+              >
+                <Slot
+                  name="icon"
+                  active={mixin.value == index}
+                  item={item}
+                  index={index}
+                >
+                  <ImageIcon
+                    className="ant-tab-bar"
+                    image={mixin.value === index ? item.activeIcon : item.icon}
+                  />
+                </Slot>
+              </AntBadge>
+            ) : (
+              <Slot
+                name="icon"
+                active={mixin.value == index}
+                item={item}
+                index={index}
+              >
+                <ImageIcon
+                  className="ant-tab-bar"
+                  image={mixin.value === index ? item.activeIcon : item.icon}
+                />
+              </Slot>
+            )}
+            <View class="ant-tab-bar-text-wrap">
+              {/* #if ALIPAY */}
+              <Slot
+                name="text"
+                active={mixin.value === index}
+                item={item}
+                index={index}
+              >
+                {/* #endif */}
+
+                <Text class="ant-tab-bar-text">{item.text}</Text>
+
+                {/* #if ALIPAY */}
+              </Slot>
+              {/* #endif */}
+            </View>
+          </View>
+        ))}
+      </View>
+    </View>
+  </Component>
+);
