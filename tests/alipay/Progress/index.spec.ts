47fd8d8e6d18abbaa903737c1868e7840ccaaae7
DiamondYuan <fandi.yfd@antgroup.com>
2024-01-24 11:56:42 +0800

    feat: 新增三个组件的单元测试

---
+++ tests/alipay/Progress/index.spec.ts
@@ -0,0 +1,123 @@
+/* eslint-disable prefer-rest-params */
+import { getInstance, sleep } from 'tests/utils';
+import { expect, it, vi } from 'vitest';
+import { IProgressBarProps } from '../../../src/Progress/props';
+
+class MockCanvas {
+  public commands: Array<[string, any[]]> = [];
+  constructor(public id: string) {
+    this.fakeCommand('clearRect');
+    this.fakeCommand('beginPath');
+    this.fakeCommand('setLineCap');
+    this.fakeCommand('arc');
+    this.fakeCommand('stroke');
+    this.fakeCommand('draw');
+  }
+
+  set imageSmoothingEnabled(v: boolean) {
+    this.commands.push(['imageSmoothingEnabled', [v]]);
+  }
+  set lineWidth(width: number) {
+    this.commands.push(['lineWidth', [width]]);
+  }
+  set strokeStyle(color: string) {
+    this.commands.push(['strokeStyle', [color]]);
+  }
+  set imageSmoothingQuality(color: string) {
+    this.commands.push(['imageSmoothingQuality', [color]]);
+  }
+  private fakeCommand(funcName) {
+    this[funcName] = (...args: any[]) => {
+      this.commands.push([funcName, args]);
+    };
+  }
+}
+
+function createProgress(props: Partial<IProgressBarProps>) {
+  const getSystemInfo = vi.fn().mockImplementation(() => {
+    return { pixelRatio: 2 };
+  });
+  const canvasMap = new Map<string, MockCanvas>();
+  const my = {
+    canIUse() {
+      return true;
+    },
+    getSystemInfo,
+    createCanvasContext(id: string) {
+      if (!canvasMap.has(id)) {
+        canvasMap.set(id, new MockCanvas(id));
+      }
+      return canvasMap.get(id);
+    },
+  };
+  const instance = getInstance(
+    'Progress',
+    {
+      ...props,
+    },
+    my
+  );
+
+  return { instance, canvasMap };
+}
+
+it('测试 lint', () => {
+  const { instance } = createProgress({});
+  instance.setProps({
+    percent: 50,
+  });
+  expect(instance.getData().curProgress).toBe(50);
+});
+
+it('测试默认绘图', async () => {
+  const { instance, canvasMap } = createProgress({
+    type: 'circle',
+  });
+  instance.setProps({
+    percent: 50,
+  });
+  await sleep(600);
+  expect(instance.getData().curProgress).toBe(50);
+  expect(canvasMap.size).toBe(1);
+  expect(
+    JSON.stringify(Array.from(canvasMap.values())[0].commands, null, 2)
+  ).toMatchFileSnapshot('snap.json');
+  expect(instance.getData().canvasWidth).toBe(200);
+});
+
+it('测试 speed strokeColor trailColor', async () => {
+  const { instance, canvasMap } = createProgress({
+    type: 'circle',
+  });
+  instance.setProps({
+    percent: 50,
+    speed: 60,
+    strokeColor: 'red',
+    trailColor: 'blue',
+  });
+  await sleep(600);
+  expect(instance.getData().curProgress).toBe(50);
+  expect(canvasMap.size).toBe(1);
+  expect(
+    JSON.stringify(Array.from(canvasMap.values())[0].commands, null, 2)
+  ).toMatchFileSnapshot('snap-speed-12.json');
+  expect(instance.getData().canvasWidth).toBe(200);
+});
+
+it('测试 speed strokeColor trailColor', async () => {
+  const { instance, canvasMap } = createProgress({
+    type: 'circle',
+    percent: 50,
+  });
+  await sleep(600);
+  instance.setProps({
+    percent: 20,
+  });
+  await sleep(600);
+  expect(instance.getData().curProgress).toBe(20);
+  expect(canvasMap.size).toBe(1);
+  expect(
+    JSON.stringify(Array.from(canvasMap.values())[0].commands, null, 2)
+  ).toMatchFileSnapshot('snap-speed-0-50-20.json');
+  expect(instance.getData().canvasWidth).toBe(200);
+});
