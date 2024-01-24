6e60fd9235d4388ae3a7a5acb9c7c3e9e7a7e8a6
DiamondYuan <fandi.yfd@antgroup.com>
2024-01-24 16:49:27 +0800

    feat: 重构 actionsheet 组件

--- compiled/alipay/src/ActionSheet/index.ts
+++ compiled/alipay/src/ActionSheet/index.ts
@@ -1,22 +1,23 @@
-import { ActionSheetDefaultProps } from './props';
-import fmtEvent from '../_util/fmtEvent';
+import { useEvent } from 'functional-mini/component';
 import '../_util/assert-component2';
+import { mountComponent } from '../_util/component';
+import { useComponentEvent } from '../_util/hooks/useComponentEvent';
+import { ActionSheetDefaultProps, IActionSheetProps } from './props';
 
-Component({
-  props: ActionSheetDefaultProps,
-  methods: {
-    onAction(e) {
-      const { item, index } = e.target.dataset;
-      if (item?.disabled) return;
-      const { onClose, onAction } = this.props;
-      const event = fmtEvent(this.props, e);
-      onClose?.(event);
-      onAction?.(item, index, event);
-    },
-    onClose(e) {
-      const { onClose } = this.props;
-      const event = fmtEvent(this.props, e);
-      onClose?.(event);
-    },
-  },
-});
+const ActionSheet = (props: IActionSheetProps) => {
+  const { triggerEventOnly, triggerEventValues } = useComponentEvent(props);
+
+  useEvent('onAction', (e) => {
+    const { item, index } = e.target.dataset;
+    if (item?.disabled) return;
+    triggerEventOnly('close', e);
+    triggerEventValues('action', [item, index], e);
+  });
+  useEvent('onClose', (e) => {
+    triggerEventOnly('close', e);
+  });
+
+  return {};
+};
+
+mountComponent(ActionSheet, ActionSheetDefaultProps);
