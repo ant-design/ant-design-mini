26aef70322922ca59517991672266cd4e84719c4
DiamondYuan <fandi.yfd@antgroup.com>
2024-01-25 20:28:22 +0800

    feat: 更新 RareWordsKeyboard 的测试

--- tests/alipay/RareWordsKeyboard/__tests__/index.test.ts
+++ tests/alipay/RareWordsKeyboard/__tests__/index.test.ts
@@ -1,53 +1,34 @@
 import fmtEvent from 'compiled-alipay/_util/fmtEvent';
 import { getInstance, sleep } from 'tests/utils';
 import { describe, it, expect, vi } from 'vitest';
+import { createSelectorQueryFactory } from 'tests/selector-query';
 
 describe('rare-words-keyboard', () => {
   const my = {
-    createSelectorQuery() {
+    createSelectorQuery: createSelectorQueryFactory((id) => {
       return {
-        select() {
-          return {
-            boundingClientRect() {
-              return {
-                select() {
-                  return {
-                    boundingClientRect() {
-                      return {
-                        exec(callback) {
-                          callback([
-                            {
-                              id: '',
-                              dataset: {},
-                              left: -9999,
-                              right: -9962.5,
-                              top: -9246,
-                              bottom: -9226.8125,
-                              width: 36.5,
-                              height: 19.1875,
-                            },
-                            {
-                              id: '',
-                              dataset: {},
-                              left: 4,
-                              right: 370,
-                              top: 763,
-                              bottom: 791,
-                              width: 366,
-                              height: 28,
-                            },
-                          ]);
-                        },
-                      };
-                    },
-                  };
-                },
-              };
-            },
-          };
+        '.ant-rare-words-keyboard-match_words_hidden': {
+          id: '',
+          dataset: {},
+          left: -9999,
+          right: -9962.5,
+          top: -9246,
+          bottom: -9226.8125,
+          width: 36.5,
+          height: 19.1875,
         },
-      };
-    },
+        '.ant-rare-words-keyboard-match_words_inner': {
+          id: '',
+          dataset: {},
+          left: 4,
+          right: 370,
+          top: 763,
+          bottom: 791,
+          width: 366,
+          height: 28,
+        },
+      }[id];
+    }),
   };
   it('rare-words-keyboard maxDisplayNum', async () => {
     const instance = getInstance(
@@ -175,5 +156,71 @@ describe('rare-words-keyboard', () => {
     await new Promise((r) => setTimeout(r, 0));
     expect(instance.getData().loading).toBe(false);
     expect(instance.getData().showErrorPage).toBe(false);
+  });
+
+  it('test hanleLookMore', async () => {
+    const instance = getInstance(
+      'RareWordsKeyboard',
+      {
+        visible: true,
+        showMask: true,
+      },
+      {
+        ...my,
+        loadFontFace: (option) => {
+          option.success();
+        },
+      }
+    );
+    await sleep(20);
+    instance.callMethod('handleKeyClick', fmtEvent({ 'data-value': 'T' }));
+    await sleep(20);
+    instance.callMethod('handleKeyClick', fmtEvent({ 'data-value': 'U' }));
+    await sleep(20);
+    instance.callMethod('hanleLookMore');
+    await sleep(20);
+    expect(instance.getData().showMoreWords).toBe(true);
+    expect(instance.getData().displayStr).toBe('TU');
+    expect(
+      instance.getData().matchWordsList.map((o) => o.charId)
+    ).toMatchFileSnapshot('tu.txt');
+    instance.callMethod('hanleLookMore');
+    await sleep(20);
+    expect(instance.getData().showMoreWords).toBe(false);
+    await sleep(20);
+    instance.callMethod('handleKeyClick', fmtEvent({ 'data-value': 'I' }));
+    await sleep(20);
+    expect(instance.getData().matchWordsList.length).toEqual(4);
+    await sleep(20);
+    instance.callMethod('hanleLookMore');
+    await sleep(20);
+    expect(instance.getData().matchWordsList.length).toEqual(0);
+    expect(instance.getData().inputValue).toEqual([]);
+  });
+
+  it('test handleDelete', async () => {
+    const instance = getInstance(
+      'RareWordsKeyboard',
+      {
+        visible: true,
+        showMask: true,
+      },
+      {
+        ...my,
+        loadFontFace: (option) => {
+          option.success();
+        },
+      }
+    );
+    await sleep(20);
+    instance.callMethod('handleKeyClick', fmtEvent({ 'data-value': 'T' }));
+    await sleep(20);
+    instance.callMethod('handleKeyClick', fmtEvent({ 'data-value': 'T' }));
+    expect(instance.getData().inputValue).toEqual(['T', 'T']);
+    instance.callMethod('handleDelete');
+    await sleep(20);
+    instance.callMethod('handleDelete');
+    instance.callMethod('handleDelete');
+    expect(instance.getData().inputValue).toEqual([]);
   });
 });
