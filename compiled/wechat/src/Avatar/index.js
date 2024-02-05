23fffac5287d397d77f6e836cd03532f24702f00
一杉 <imsenyu@gmail.com>
2024-02-05 15:27:35 +0800

    perf: 减少 Avatar 数据发送量

--- compiled/wechat/src/Avatar/index.js
+++ compiled/wechat/src/Avatar/index.js
@@ -1,7 +1,4 @@
 import '../_util/assert-component2';
-import { mountComponent } from '../_util/component';
+import { Component } from '../_util/simply';
 import { AvatarDefaultProps } from './props';
-var Avatar = function () {
-    //
-};
-mountComponent(Avatar, AvatarDefaultProps);
+Component(AvatarDefaultProps);
