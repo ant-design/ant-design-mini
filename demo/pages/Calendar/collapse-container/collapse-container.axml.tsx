0045ac41fea514e66ba00c2eca5c60dfda7ee02c
DiamondYuan <fandi.yfd@antgroup.com>
2024-02-07 16:04:49 +0800

    fix: 更新更多页面

--- demo/pages/Calendar/collapse-container/collapse-container.axml.tsx
+++ demo/pages/Calendar/collapse-container/collapse-container.axml.tsx
@@ -1,12 +1,12 @@
 import { InternalData, TSXMLProps, View, Text, Slot } from 'tsxml';
-import Container from '../../../../src/Container/index.axml';
+import AntContainer from '../../../../src/Container/index.axml';
 import { Props } from './collapse-container';
 
 export default (
   _: TSXMLProps<Props>,
   { containerTitle, collapse }: InternalData
 ) => (
-  <Container title={containerTitle}>
+  <AntContainer title={containerTitle}>
     <View slot="headerRight" onTap="handleToggle" data-id="demo1">
       {!collapse ? <Text>收起</Text> : <Text>展示</Text>}
     </View>
@@ -20,5 +20,5 @@ export default (
         <View class="placeholder">展示</View>
       </View>
     )}
-  </Container>
+  </AntContainer>
 );
