f8e953ec181219eb66215bc5ea0d11d7b46115d4
DiamondYuan <fandi.yfd@antgroup.com>
2024-02-07 15:58:09 +0800

    fix: 更新更多页面

--- demo/pages/NoticeBar/index.axml.tsx
+++ demo/pages/NoticeBar/index.axml.tsx
@@ -1,26 +1,26 @@
 import { InternalData, Page, View } from 'tsxml';
-import Notice from '../../../src/NoticeBar/index.axml';
-import Container from '../../../src/Container/index.axml';
+import AntNotice from '../../../src/NoticeBar/index.axml';
+import AntContainer from '../../../src/Container/index.axml';
 
 export default ({ typeList }: InternalData) => (
   <Page>
-    <Container title="基础用法">
+    <AntContainer title="基础用法">
       {typeList.map((item) => (
-        <Notice style="margin-bottom: 8px" type={item}>
+        <AntNotice style="margin-bottom: 8px" type={item}>
           {item}
-        </Notice>
+        </AntNotice>
       ))}
-    </Container>
+    </AntContainer>
 
-    <Container title="可关闭通告栏">
-      <Notice onTap="handleClose" mode="closeable">
+    <AntContainer title="可关闭通告栏">
+      <AntNotice onTap="handleClose" mode="closeable">
         这条通知可以关闭
-      </Notice>
-    </Container>
+      </AntNotice>
+    </AntContainer>
 
-    <Container title="可滚动通告栏">
+    <AntContainer title="可滚动通告栏">
       {typeList.map((item) => (
-        <Notice
+        <AntNotice
           type={item}
           style="margin-bottom: 8px"
           enableMarquee={true}
@@ -29,13 +29,13 @@ export default ({ typeList }: InternalData) => (
           mode="link"
         >
           文本溢出时，开启循环滚动。文字不够继续添加文字凑数。
-        </Notice>
+        </AntNotice>
       ))}
-    </Container>
+    </AntContainer>
 
-    <Container title="可滚动通告栏(不循环)">
+    <AntContainer title="可滚动通告栏(不循环)">
       {typeList.map((item) => (
-        <Notice
+        <AntNotice
           type={item}
           style="margin-bottom: 8px"
           enableMarquee={true}
@@ -43,41 +43,41 @@ export default ({ typeList }: InternalData) => (
           mode="link"
         >
           文本溢出时，开启循环滚动。文字不够继续添加文字凑数。
-        </Notice>
+        </AntNotice>
       ))}
-    </Container>
+    </AntContainer>
 
-    <Container title="自定义通告栏">
-      <Notice style="margin-bottom: 8px">
+    <AntContainer title="自定义通告栏">
+      <AntNotice style="margin-bottom: 8px">
         不展示图标
         <View slot="icon" />
-      </Notice>
+      </AntNotice>
 
-      <Notice
+      <AntNotice
         type="primary"
         icon="GlobalOutline"
         style="margin-bottom: 8px"
         mode="link"
       >
         自定义左侧图标
-      </Notice>
+      </AntNotice>
 
-      <Notice
+      <AntNotice
         type="primary"
         icon="https://gw.alipayobjects.com/mdn/rms_ce4c6f/afts/img/A*XMCgSYx3f50AAAAAAAAAAABkARQnAQ"
         style="margin-bottom: 8px"
         mode="link"
       >
         自定义左侧图标图片
-      </Notice>
+      </AntNotice>
 
-      <Notice mode="link" style="margin-bottom: 8px" onTap="handleTapLink">
+      <AntNotice mode="link" style="margin-bottom: 8px" onTap="handleTapLink">
         自定义右侧按钮
         <View slot="extra" class="extra">
           <View onTap="handleTapAction">不再提示</View>
           <View onTap="handleTapAction">查看详情</View>
         </View>
-      </Notice>
-    </Container>
+      </AntNotice>
+    </AntContainer>
   </Page>
 );
