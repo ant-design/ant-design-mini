1df095ef1683bd40e108d3f20b04dc55683bb5cd
rayhomie <1572801584@qq.com>
2024-04-23 21:09:34 +0800

    feat: NumberKeyboard去hook

--- compiled/wechat/src/NumberKeyboard/index.js
+++ compiled/wechat/src/NumberKeyboard/index.js
@@ -1,66 +1,71 @@
-import { useEffect } from 'functional-mini/compat';
-import { useEvent, useState } from 'functional-mini/component';
-import { mountComponent } from '../_util/component';
-import { useComponentEvent } from '../_util/hooks/useComponentEvent';
+import { Component, triggerEvent, triggerEventOnly, getValueFromProps, } from '../_util/simply';
 import { NumberKeyboardFunctionalProps } from './props';
-import { useEvent as useStableCallback } from '../_util/hooks/useEvent';
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var NumberKeyboard = function (props) {
-    var numArr = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-    ];
-    var _a = useState([]), randomArr = _a[0], setRandomArr = _a[1];
-    var _b = useComponentEvent(props), triggerEventOnly = _b.triggerEventOnly, triggerEvent = _b.triggerEvent;
-    var setRandom = useStableCallback(function () {
-        if (props.random) {
+Component(NumberKeyboardFunctionalProps, {
+    catchAppearModal: function () {
+        this.setRandom();
+    },
+    setRandom: function () {
+        if (getValueFromProps(this, 'random')) {
             var newArr = arr.sort(function () {
                 return Math.random() - 0.5;
             });
-            setRandomArr([
-                newArr.slice(0, 3),
-                newArr.slice(3, 6),
-                newArr.slice(6, 9),
-            ]);
+            this.setData({
+                randomArr: [
+                    newArr.slice(0, 3),
+                    newArr.slice(3, 6),
+                    newArr.slice(6, 9),
+                ],
+            });
         }
-    });
-    var vibrate = useStableCallback(function () {
-        if (props.vibrate && typeof my !== 'undefined') {
-            my.canIUse('vibrateShort') && my.vibrateShort();
-        }
-    });
-    useEvent('handleHide', function () {
-        triggerEventOnly('close');
-    });
-    useEvent('tapButton', function (e) {
-        var value = props.value, confirmDisabled = props.confirmDisabled;
-        vibrate();
+    },
+    tapButton: function (e) {
+        var _a = getValueFromProps(this, [
+            'value',
+            'confirmDisabled',
+        ]), value = _a[0], confirmDisabled = _a[1];
+        this.vibrate();
         var _key = e.currentTarget.dataset.key;
         var _val = "".concat(value);
         // 回退
         if (_key === 'del') {
-            triggerEvent('change', "".concat(_val.substr(0, _val.length - 1)));
+            triggerEvent(this, 'change', "".concat(_val.substr(0, _val.length - 1)), e);
             return;
         }
         if (_key !== 'del' && _key !== 'enter') {
-            triggerEvent('change', "".concat(_val).concat(_key));
+            triggerEvent(this, 'change', "".concat(_val).concat(_key), e);
         }
         if (_key === 'enter' && !confirmDisabled) {
-            triggerEventOnly('close');
-            triggerEventOnly('confirm');
-            triggerEventOnly('close'); // 保持和之前一样的逻辑
+            this.onClickEnter();
+            triggerEventOnly(this, 'close');
         }
-    });
-    useEvent('catchAppearModal', function () {
-        setRandom();
-    });
-    useEffect(function () {
-        setRandom();
-    }, []);
-    return {
-        randomArr: randomArr,
-        numArr: numArr,
-    };
-};
-mountComponent(NumberKeyboard, NumberKeyboardFunctionalProps);
+    },
+    // 隐藏键盘，失去焦点
+    handleHide: function () {
+        triggerEventOnly(this, 'close');
+    },
+    onClickEnter: function () {
+        var confirmDisabled = getValueFromProps(this, 'confirmDisabled');
+        if (confirmDisabled)
+            return;
+        this.handleHide();
+        triggerEventOnly(this, 'confirm');
+    },
+    // 振动反馈
+    vibrate: function () {
+        if (getValueFromProps(this, 'vibrate') && typeof my !== 'undefined') {
+            my.canIUse('vibrateShort') && my.vibrateShort();
+        }
+    },
+}, {
+    numArr: [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ],
+    randomArr: [],
+}, undefined, {
+    attached: function () {
+        this.setRandom();
+    },
+});
--- compiled/wechat/src/NumberKeyboard/props.js
+++ compiled/wechat/src/NumberKeyboard/props.js
@@ -1,19 +1,4 @@
 export var NumberKeyboardDefaultProps = {
-    className: '',
-    value: '',
-    visible: false,
-    safeArea: true,
-    closeable: false,
-    random: false,
-    vibrate: false,
-    point: true,
-    confirmDisabled: false,
-    confirmText: '',
-    onChange: function (val) { },
-    onConfirm: function () { },
-    onClose: function () { }, // 关闭
-};
-export var NumberKeyboardFunctionalProps = {
     closeable: false,
     point: true,
     confirmText: '',
