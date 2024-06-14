63bcc75e4e541f75197923211fa2a374cf90cb92
zaSang <sangza123@163.com>
2024-06-14 16:08:04 +0800

    fix(native)

--- src/RadioGroup/RadioItem/index.ts
+++ src/RadioGroup/RadioItem/index.ts
@@ -44,15 +44,5 @@ Component({
         componentContext.update(key, this.props.value);
       }
     },
-    handleNativeFallback() {
-      if (this.renderer === 'native' && !this.data._disabled) {
-        const fakeEvent = {
-          detail: {
-            value: true,
-          }
-        };
-        this.onItemChange(fakeEvent);
-      }
-    },
   },
 });
