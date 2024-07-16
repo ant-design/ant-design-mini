824c68b9404acd2bb8111f0d4559eff27e17ee95
rayhomie <1572801584@qq.com>
2024-07-16 20:57:14 +0800

    feat: 删除demo文件夹中axmltsx

--- demo/pages/Calendar/collapse-container/cn-day/cn-day.axml.tsx
+++
@@ -1,7 +0,0 @@
-import { InternalData, View } from 'tsxml';
-
-export default ({ cnday, festival, unset }: InternalData) => (
-  <View class={`cn-day ${festival ? 'festival' : ''} ${unset ? 'unset' : ''}`}>
-    {cnday}
-  </View>
-);
