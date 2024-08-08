930db282daaa96d39cf5118710aa23ed3f30164c
homi <1572801584@qq.com>
2024-08-08 10:25:01 +0800

    @alipay/axml-transverter 依赖替换 (#1261)
    * feat: indexbar异步加载数据时，异常处理
    
    * feat: 替换axml-transverter包
    
    * feat: 替换axml-transverter包

--- scripts/axml/index.ts
+++ scripts/axml/index.ts
@@ -1,4 +1,4 @@
-import { TransformCompiler as axmlParser } from 'axml-transverter';
+import { TransformCompiler as axmlParser } from '@alipay/axml-transverter';
 
 export { wechatCustomMapping } from './mapping';
 
