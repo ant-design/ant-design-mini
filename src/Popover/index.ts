8c71ead6c35745fb0c8b9321a2633020a55f8663
weifang.jiagweifan <weifang.jiagweifan@antgroup.com>
2024-09-19 16:41:08 +0800

    feat: popover 样式升级

--- src/Popover/index.ts
+++ src/Popover/index.ts
@@ -73,6 +73,10 @@ Component(
       }
       triggerEvent(this, 'visibleChange', value, e);
     },
+    onTapAction() {
+      this.onVisibleChange();
+      this.props.onTapAction();
+    }
   },
   {
     adjustedPlacement: '',
