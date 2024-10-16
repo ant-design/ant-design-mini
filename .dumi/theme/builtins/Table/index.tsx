3bec1e320d287c80e07bd1f2637bcbebfee096ce
rayhomie <1572801584@qq.com>
2024-10-16 11:48:44 +0800

    feat: add markdown table render

--- .dumi/theme/builtins/Table/index.tsx
+++ .dumi/theme/builtins/Table/index.tsx
@@ -1,74 +1,102 @@
-import { Table as AntdTable } from 'antd';
-import React, { useContext, useMemo } from 'react';
+import throttle from 'lodash.throttle';
+import React, {
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import type { SiteContextProps } from '../../slots/SiteContext';
 import SiteContext from '../../slots/SiteContext';
 import './index.less';
 
 interface IProps {
-  alipayContent: string;
-  wechatContent?: string;
-}
-// 解析markdown语法，并适配 Antd Table
-function parseMarkdownToAntdTable(markdown: string) {
-  const lines = markdown.trim().split('\n');
-
-  // 获取表头信息
-  const headers = lines[0]
-    .split('|')
-    .map((header) => header.trim())
-    .filter(Boolean);
-
-  // 创建 columns 数组
-  const columns = headers.map((header, index) => ({
-    title: header,
-    dataIndex: index.toString(),
-    key: index.toString(),
-  }));
-
-  // 创建 dataSource 数组
-  const dataSource = lines.slice(2).map((line, rowIndex) => {
-    const cells = line.split('|').map((cell) => cell.trim());
-    const row = { key: rowIndex.toString() };
-    cells.forEach((cell, index) => {
-      row[(index - 1).toString()] = cell;
-    });
-    return row;
-  });
-
-  return { columns, dataSource };
+  children: ReactNode;
 }
 
-// 将 -$u 后面的字母改为大写
-function capitalizeAfterSubU(text) {
-  return text.replace(/-\$u(\w)/g, (_, letter) => letter.toUpperCase());
-}
+const MARKUP_REGEX = {
+  all: /#ALIPAY#|#WECHAT#/, // 支持的所有平台
+  alipay: /#WECHAT#/, // 支付宝平台时，需要去掉微信标记表格项
+  wechat: /#ALIPAY#/, // 微信平台时，需要去掉支付宝标记表格项
+};
 
-const Table: React.FC<IProps> = ({ alipayContent, wechatContent }) => {
+const Table: React.FC<IProps> = ({ children, ...props }) => {
   const { platform } = useContext<SiteContextProps>(SiteContext);
 
-  const mergeContent = { alipayContent, wechatContent };
+  const container = useRef<HTMLDivElement>(null);
+  const [leftFolded, setLeftFolded] = useState(false);
+  const [rightFolded, setRightFolded] = useState(false);
 
-  const content = useMemo(
-    () => mergeContent?.[`${platform}Content`] || '',
-    [alipayContent, wechatContent, platform]
+  // watch content scroll to render folded shadow
+  useEffect(() => {
+    const elm = container.current;
+    if (elm) {
+      const handler = throttle(() => {
+        setLeftFolded(elm.scrollLeft > 0);
+        setRightFolded(elm.scrollLeft < elm.scrollWidth - elm.offsetWidth);
+      }, 100);
+
+      handler();
+      elm.addEventListener('scroll', handler);
+      window.addEventListener('resize', handler);
+
+      return () => {
+        elm.removeEventListener('scroll', handler);
+        window.removeEventListener('resize', handler);
+      };
+    }
+  }, []);
+
+  // 处理表格标签，做条件编译
+  const processReactNode = useCallback(
+    (node) => {
+      if (Array.isArray(node)) {
+        let containsAlipay = false;
+
+        // 遍历节点数组，检测是否包含 '#ALIPAY#'
+        node.forEach((child) => {
+          if (typeof child === 'string' && MARKUP_REGEX[platform].test(child)) {
+            containsAlipay = true;
+          } else if (
+            React.isValidElement(child) &&
+            typeof child.props.children === 'string' &&
+            MARKUP_REGEX[platform].test(child.props.children)
+          ) {
+            containsAlipay = true;
+          }
+        });
+
+        // 如果包含 '#ALIPAY#'，返回空数组以舍弃整个节点和其兄弟
+        return containsAlipay
+          ? []
+          : node.map(processReactNode).filter((child) => child !== null);
+      } else if (React.isValidElement(node)) {
+        const { children } = node.props;
+        const processedChildren = processReactNode(children);
+
+        return React.cloneElement(node, {
+          children: processedChildren,
+        });
+      } else {
+        node = node.replace(MARKUP_REGEX.all, '');
+        return node;
+      }
+    },
+    [children, platform, MARKUP_REGEX]
   );
-
-  const { columns, dataSource } = useMemo(
-    () => parseMarkdownToAntdTable(capitalizeAfterSubU(content)),
-    [content]
-  );
-
-  console.log(content, columns, dataSource);
-  if (!content) {
-    return null;
-  }
 
   return (
-    <AntdTable
-      columns={columns}
-      dataSource={dataSource}
-      pagination={false}
-    ></AntdTable>
+    <div className="dumi-default-table">
+      <div
+        className="dumi-default-table-content"
+        ref={container}
+        data-left-folded={leftFolded || undefined}
+        data-right-folded={rightFolded || undefined}
+      >
+        <table {...props}>{processReactNode(children)}</table>
+      </div>
+    </div>
   );
 };
 
