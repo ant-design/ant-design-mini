3a24149ac62fd3db40b36b82a4f6956c56e5e191
shijia.sj <shijia.sj@antfin.com>
2024-11-01 11:18:40 +0800

    feat: add more locales

--- src/_locale/zh-HK.ts
+++ src/_locale/zh-HK.ts
@@ -4,49 +4,49 @@ import type { Locale } from '.';
 const zhHK: Locale = {
   // locales for all components
   global: {
-    placeholder: '请选择香港',
-    emptyText: '暂无数据',
-    okText: '确定',
+    placeholder: '請選擇',
+    emptyText: '暫無數據',
+    okText: '確定',
     cancelText: '取消',
   },
   calendar: {
     weekdayNames: ['一', '二', '三', '四', '五', '六', '日'],
     title: 'YYYY年MM月',
     today: '今日',
-    start: '开始',
-    end: '结束',
-    startAndEnd: '开始/结束',
+    start: '開始',
+    end: '結束',
+    startAndEnd: '開始/結束',
   },
   rangePicker: {
-    startPlaceholder: '未选择',
-    endPlaceholder: '未选择',
+    startPlaceholder: '未選擇',
+    endPlaceholder: '未選擇',
   },
   guideTour: {
     gotItText: '知道了',
     nextStepText: '下一步',
     prevStepText: '上一步',
-    jumpText: '跳过',
+    jumpText: '跳過',
   },
   imageUpload: {
-    uploadingText: '上传中',
-    uploadfailedText: '上传失败',
+    uploadingText: '上傳中',
+    uploadfailedText: '上傳失敗',
   },
   pageContainer: {
     failed: {
-      title: '页面遇到一些小问题',
-      message: '待会来试试',
+      title: '頁面遇到一些小問題',
+      message: '待會來試試',
     },
     disconnected: {
-      title: '网络有点忙',
-      message: '动动手指帮忙修复',
+      title: '網絡有點忙',
+      message: '動動手指幫忙修復',
     },
     empty: {
-      title: '这里什么也没有',
+      title: '這裡什麼也沒有',
       message: '看看其它吧',
     },
     busy: {
-      title: '前方拥堵',
-      message: '刷新试试',
+      title: '前方擁堵',
+      message: '刷新試試',
     },
   },
 };
