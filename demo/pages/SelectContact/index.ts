34d4a48463002ec40f960aeab83d831946865a50
shijia.sj <shijia.sj@antfin.com>
2024-11-14 23:50:17 +0800

    feat: 优化选人组件

---
+++ demo/pages/SelectContact/index.ts
@@ -0,0 +1,21 @@
+Page({
+  data: {},
+  onLoad: () => {
+    // Android 必须设置 canPullDown 为 false，否则滚动会有问题
+    my.setCanPullDown({
+      canPullDown: false,
+    });
+  },
+  onSelect: (user) => {
+    console.log('user', user);
+    my.confirm({
+      content: JSON.stringify(user),
+    });
+  },
+  onDeleteRecommendUserError(error) {
+    console.error('onDeleteRecommendUserError', error);
+  },
+  onDeleteRecommendUser(userInfo) {
+    console.log('onDeleteRecommendUser', userInfo);
+  },
+});
