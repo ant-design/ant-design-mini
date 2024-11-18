0989e27057b98b0ac0ddbbc8eb0f79326762bf4e
rayhomie <1572801584@qq.com>
2024-11-18 15:36:01 +0800

    feat: wx check

--- demo/pages/Form/FormCustom/checklist/index.sjs.ts
+++ demo/pages/Form/FormCustom/checklist/index.sjs.ts
@@ -1,5 +1,5 @@
 function getTitle(options, value) {
-  const obj = options.find(item => item.value === value);
+  const obj = options.find((item) => item.value === value);
   if (obj) {
     return obj.title;
   }
