01249e72057ab9e18dc18f43b004ce1adadf56dc
homi <1572801584@qq.com>
2025-01-08 11:08:30 +0800

    Add wx:key (#1370)
    * fix: inline button style fix in wechat platform
    
    * docs: translate doc
    
    * docs: translate doc
    
    * feat: wx:key add

--- demo/pages/Empty/index.ts
+++ demo/pages/Empty/index.ts
@@ -1,24 +1,28 @@
 const BUTTON_LISt = [
   {
     type: 'primary',
-    text: '刷新重试'
+    text: '刷新重试',
   },
   {
     type: 'default',
-    text: '网络诊断'
+    text: '网络诊断',
   },
 ];
 Page({
   data: {
-    imageUrl: 'https://mdn.alipayobjects.com/huamei_mnxlps/afts/img/A*oV92T4FXR64AAAAAAAAAAAAADkqGAQ/original',
+    imageUrl:
+      'https://mdn.alipayobjects.com/huamei_mnxlps/afts/img/A*oV92T4FXR64AAAAAAAAAAAAADkqGAQ/original',
     mode: 'page',
-    modeList: [{
-      label: '全页空状态',
-      value: 'page',
-    }, {
-      label: '局部空状态',
-      value: 'section',
-    }],
+    modeList: [
+      {
+        label: '全页空状态',
+        value: 'page',
+      },
+      {
+        label: '局部空状态',
+        value: 'section',
+      },
+    ],
     horizontalButtonInfo: {
       layout: 'horizontal',
       list: BUTTON_LISt,
@@ -30,6 +34,12 @@ Page({
   },
   onClickButton(buttonInfo) {
     console.log('onClickButton buttonInfo: ', buttonInfo);
+    /// #if ALIPAY
     my.showToast({ content: `点击了按钮「${buttonInfo.text}」` });
+    /// #endif
+    /// #if WECHAT
+    // @ts-ignore
+    wx.showToast({ title: `点击了按钮「${buttonInfo.detail.text}」` });
+    /// #endif
   },
 });
