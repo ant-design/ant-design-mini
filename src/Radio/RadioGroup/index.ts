f1896f434750c7e31f62ce44c9d6c49c8cc855b1
lizhihongle <205801051+lizhihongle@users.noreply.github.com>
2025-04-06 21:57:17 +0800

    fix: resolve RadioGroup can‘t trigger change event in alipay SDK 1.x version

--- src/Radio/RadioGroup/index.ts
+++ src/Radio/RadioGroup/index.ts
@@ -1,25 +1,16 @@
 import mixinValue from '../../mixins/value';
-import { Component, getValueFromProps, triggerEvent } from '../../_util/simply';
+import { Component, triggerEvent } from '../../_util/simply';
 import { RadioGroupDefaultProps } from './props';
 
 Component({
   props: RadioGroupDefaultProps,
   methods: {
-    onChange(_, e) {
-      let event;
-      /// #if ALIPAY
-      event = e;
-      /// #endif
-      /// #if WECHAT
-      event = _;
-      /// #endif
-      const index = event.currentTarget.dataset.index;
-      const options = getValueFromProps(this, 'options');
-      const value = options[index].value;
+    onChange(e) {
+      const value = e.detail.value;
       if (!this.isControlled()) {
         this.update(value);
       }
-      triggerEvent(this, 'change', value, event);
+      triggerEvent(this, 'change', value, e);
     },
   },
   mixins: [mixinValue()],
