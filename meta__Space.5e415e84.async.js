41769ed643e31ae5e3527298f887836b05e453d7
rayhomie <1572801584@qq.com>
2025-04-11 15:23:02 +0800

    Updates

---
+++ meta__Space.5e415e84.async.js
@@ -0,0 +1,36 @@
+"use strict";(self.webpackChunkantd_mini=self.webpackChunkantd_mini||[]).push([[6122],{39438:function(d,n,a){a.r(n),a.d(n,{demos:function(){return I}});var t=a(67294),e=a(33628),I={"src-space-demo-space":{component:t.memo(t.lazy(function(){return a.e(2433).then(a.bind(a,31319))})),asset:{type:"BLOCK",id:"src-space-demo-space",refAtomIds:[],dependencies:{},entry:"index.ts"},context:{},renderOpts:void 0}}},99797:function(d,n,a){a.r(n),a.d(n,{texts:function(){return e}});var t=a(33628);const e=[{value:"\u8BBE\u7F6E\u7EC4\u4EF6\u4E4B\u95F4\u7684\u95F4\u8DDD\u3002",paraId:0,tocIndex:0},{value:"\u907F\u514D\u7EC4\u4EF6\u7D27\u8D34\u5728\u4E00\u8D77\uFF0C\u62C9\u5F00\u7EDF\u4E00\u7684\u7A7A\u95F4\u3002",paraId:1,tocIndex:1},{value:"\u9002\u5408\u884C\u5185\u5143\u7D20\u7684\u6C34\u5E73\u95F4\u8DDD\u3002",paraId:2,tocIndex:1},{value:"\u53EF\u4EE5\u8BBE\u7F6E\u5404\u79CD\u6C34\u5E73\u5BF9\u9F50\u65B9\u5F0F\u3002",paraId:2,tocIndex:1},{value:"\u5728 ",paraId:3,tocIndex:2},{value:"index.json",paraId:3,tocIndex:2},{value:" \u4E2D\u5F15\u5165\u7EC4\u4EF6",paraId:3,tocIndex:2},{value:`"usingComponents": {
+#if ALIPAY
+  "ant-space": "antd-mini/es/Space/index"
+#endif
+#if WECHAT
+  "ant-space": "antd-mini/Space/index"
+#endif
+}
+`,paraId:4,tocIndex:2},{value:`<ant-space>
+  <ant-button type="primary">\u6309\u94AE1</ant-button>
+  <ant-button>\u6309\u94AE2</ant-button>
+  <ant-button>\u6309\u94AE3</ant-button>
+</ant-space>
+`,paraId:5,tocIndex:4},{value:`<ant-space direction="vertical">
+  <ant-button type="primary">\u6309\u94AE1</ant-button>
+  <ant-button>\u6309\u94AE2</ant-button>
+  <ant-button>\u6309\u94AE3</ant-button>
+</ant-space>
+`,paraId:6,tocIndex:5},{value:`<ant-space size="small">
+  <ant-button type="primary">\u6309\u94AE1</ant-button>
+  <ant-button>\u6309\u94AE2</ant-button>
+  <ant-button>\u6309\u94AE3</ant-button>
+</ant-space>
+`,paraId:7,tocIndex:6},{value:`<ant-space align="start">
+  <ant-button type="primary">\u6309\u94AE1</ant-button>
+  <view class="custom-box">\u9AD8\u5EA6\u4E0D\u540C\u7684\u5185\u5BB9</view>
+  <ant-button>\u6309\u94AE3</ant-button>
+</ant-space>
+`,paraId:8,tocIndex:7},{value:`<ant-space wrap>
+  <ant-button type="primary">\u6309\u94AE1</ant-button>
+  <ant-button>\u6309\u94AE2</ant-button>
+  <ant-button>\u6309\u94AE3</ant-button>
+  <ant-button>\u6309\u94AE4</ant-button>
+  <ant-button>\u6309\u94AE5</ant-button>
+</ant-space>
+`,paraId:9,tocIndex:8},{value:"\u5C5E\u6027",paraId:10,tocIndex:10},{value:"\u8BF4\u660E",paraId:10,tocIndex:10},{value:"\u7C7B\u578B",paraId:10,tocIndex:10},{value:"\u9ED8\u8BA4\u503C",paraId:10,tocIndex:10},{value:"direction",paraId:10,tocIndex:10},{value:"\u95F4\u8DDD\u65B9\u5411",paraId:10,tocIndex:10},{value:"'horizontal'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'vertical'",paraId:10,tocIndex:10},{value:"'horizontal'",paraId:10,tocIndex:10},{value:"size",paraId:10,tocIndex:10},{value:"\u95F4\u8DDD\u5927\u5C0F",paraId:10,tocIndex:10},{value:"'small'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'middle'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'large'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"number",paraId:10,tocIndex:10},{value:"'middle'",paraId:10,tocIndex:10},{value:"align",paraId:10,tocIndex:10},{value:"\u5BF9\u9F50\u65B9\u5F0F",paraId:10,tocIndex:10},{value:"'start'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'end'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'center'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'baseline'",paraId:10,tocIndex:10},{value:"'center'",paraId:10,tocIndex:10},{value:"justify",paraId:10,tocIndex:10},{value:"\u4E3B\u8F74\u5BF9\u9F50\u65B9\u5F0F",paraId:10,tocIndex:10},{value:"'start'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'end'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'center'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'space-around'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'space-between'",paraId:10,tocIndex:10},{value:"'start'",paraId:10,tocIndex:10},{value:"wrap",paraId:10,tocIndex:10},{value:"\u662F\u5426\u81EA\u52A8\u6362\u884C",paraId:10,tocIndex:10},{value:"boolean",paraId:10,tocIndex:10},{value:"false",paraId:10,tocIndex:10},{value:"className",paraId:10,tocIndex:10},{value:"\u81EA\u5B9A\u4E49\u7C7B\u540D",paraId:10,tocIndex:10},{value:"string",paraId:10,tocIndex:10},{value:"-",paraId:10,tocIndex:10},{value:"style",paraId:10,tocIndex:10},{value:"\u81EA\u5B9A\u4E49\u6837\u5F0F",paraId:10,tocIndex:10},{value:"string",paraId:10,tocIndex:10},{value:"-",paraId:10,tocIndex:10},{value:"\u7EC4\u4EF6\u63D0\u4F9B\u4E86\u4E0B\u5217 CSS \u53D8\u91CF\uFF0C\u53EF\u7528\u4E8E\u81EA\u5B9A\u4E49\u6837\u5F0F\uFF0C\u4F7F\u7528\u65B9\u6CD5\u8BF7\u53C2\u8003 ConfigProvider \u7EC4\u4EF6\u3002",paraId:11,tocIndex:12},{value:"\u53D8\u91CF\u540D",paraId:12,tocIndex:12},{value:"\u9ED8\u8BA4\u503C",paraId:12,tocIndex:12},{value:"\u6DF1\u8272\u6A21\u5F0F\u9ED8\u8BA4\u503C",paraId:12,tocIndex:12},{value:"\u5907\u6CE8",paraId:12,tocIndex:12},{value:"--space-gap",paraId:12,tocIndex:12},{value:"0.08rem",paraId:12,tocIndex:12},{value:"0.08rem",paraId:12,tocIndex:12},{value:"\u95F4\u8DDD\u5927\u5C0F",paraId:12,tocIndex:12}]}}]);
---
+++ meta__Space__en.374044c0.async.js
@@ -0,0 +1,36 @@
+"use strict";(self.webpackChunkantd_mini=self.webpackChunkantd_mini||[]).push([[9506],{53566:function(d,t,a){a.r(t),a.d(t,{demos:function(){return o}});var n=a(67294),e=a(48540),o={"src-space-demo-space":{component:n.memo(n.lazy(function(){return a.e(2433).then(a.bind(a,31319))})),asset:{type:"BLOCK",id:"src-space-demo-space",refAtomIds:[],dependencies:{},entry:"index.ts"},context:{},renderOpts:void 0}}},36426:function(d,t,a){a.r(t),a.d(t,{texts:function(){return e}});var n=a(48540);const e=[{value:"Set the spacing between components.",paraId:0,tocIndex:0},{value:"Avoid components sticking together and create uniform spacing.",paraId:1,tocIndex:1},{value:"Suitable for horizontal spacing of inline elements.",paraId:2,tocIndex:1},{value:"Various horizontal alignment methods can be set.",paraId:2,tocIndex:1},{value:"In ",paraId:3,tocIndex:2},{value:"index.json",paraId:3,tocIndex:2},{value:" Introducing Components in",paraId:3,tocIndex:2},{value:`"usingComponents": {
+#if ALIPAY
+  "ant-space": "antd-mini/es/Space/index"
+#endif
+#if WECHAT
+  "ant-space": "antd-mini/Space/index"
+#endif
+}
+`,paraId:4,tocIndex:2},{value:`<ant-space>
+  <ant-button type="primary">Button 1</ant-button>
+  <ant-button>Button 2</ant-button>
+  <ant-button>Button 3</ant-button>
+</ant-space>
+`,paraId:5,tocIndex:4},{value:`<ant-space direction="vertical">
+  <ant-button type="primary">Button 1</ant-button>
+  <ant-button>Button 2</ant-button>
+  <ant-button>Button 3</ant-button>
+</ant-space>
+`,paraId:6,tocIndex:5},{value:`<ant-space size="small">
+  <ant-button type="primary">Button 1</ant-button>
+  <ant-button>Button 2</ant-button>
+  <ant-button>Button 3</ant-button>
+</ant-space>
+`,paraId:7,tocIndex:6},{value:`<ant-space align="start">
+  <ant-button type="primary">Button 1</ant-button>
+  <view class="custom-box">Content with different height</view>
+  <ant-button>Button 3</ant-button>
+</ant-space>
+`,paraId:8,tocIndex:7},{value:`<ant-space wrap>
+  <ant-button type="primary">Button 1</ant-button>
+  <ant-button>Button 2</ant-button>
+  <ant-button>Button 3</ant-button>
+  <ant-button>Button 4</ant-button>
+  <ant-button>Button 5</ant-button>
+</ant-space>
+`,paraId:9,tocIndex:8},{value:"Property",paraId:10,tocIndex:10},{value:"Description",paraId:10,tocIndex:10},{value:"Type",paraId:10,tocIndex:10},{value:"Default Value",paraId:10,tocIndex:10},{value:"direction",paraId:10,tocIndex:10},{value:"Space direction",paraId:10,tocIndex:10},{value:"'horizontal'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'vertical'",paraId:10,tocIndex:10},{value:"'horizontal'",paraId:10,tocIndex:10},{value:"size",paraId:10,tocIndex:10},{value:"Space size",paraId:10,tocIndex:10},{value:"'small'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'middle'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'large'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"number",paraId:10,tocIndex:10},{value:"'middle'",paraId:10,tocIndex:10},{value:"align",paraId:10,tocIndex:10},{value:"Alignment",paraId:10,tocIndex:10},{value:"'start'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'end'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'center'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'baseline'",paraId:10,tocIndex:10},{value:"'center'",paraId:10,tocIndex:10},{value:"justify",paraId:10,tocIndex:10},{value:"Main axis alignment",paraId:10,tocIndex:10},{value:"'start'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'end'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'center'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'space-around'",paraId:10,tocIndex:10},{value:" | ",paraId:10,tocIndex:10},{value:"'space-between'",paraId:10,tocIndex:10},{value:"'start'",paraId:10,tocIndex:10},{value:"wrap",paraId:10,tocIndex:10},{value:"Whether to automatically wrap",paraId:10,tocIndex:10},{value:"boolean",paraId:10,tocIndex:10},{value:"false",paraId:10,tocIndex:10},{value:"className",paraId:10,tocIndex:10},{value:"Custom class name",paraId:10,tocIndex:10},{value:"string",paraId:10,tocIndex:10},{value:"-",paraId:10,tocIndex:10},{value:"style",paraId:10,tocIndex:10},{value:"Custom style",paraId:10,tocIndex:10},{value:"string",paraId:10,tocIndex:10},{value:"-",paraId:10,tocIndex:10},{value:"Component provides the following CSS variables, which can be used to customize styles. For details, see ConfigProvider Components.",paraId:11,tocIndex:12},{value:"Variable name",paraId:12,tocIndex:12},{value:"Default Value",paraId:12,tocIndex:12},{value:"Dark Mode Default",paraId:12,tocIndex:12},{value:"Remarks",paraId:12,tocIndex:12},{value:"--space-gap",paraId:12,tocIndex:12},{value:"8px",paraId:12,tocIndex:12},{value:"8px",paraId:12,tocIndex:12},{value:"Space size",paraId:12,tocIndex:12}]}}]);
