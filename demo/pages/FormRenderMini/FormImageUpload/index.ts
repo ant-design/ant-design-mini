f084b8312be1283d7beb44a9abce3afb7ba77a78
weifang.jiagweifan <weifang.jiagweifan@antgroup.com>
2025-05-08 17:17:50 +0800

    feat: 图片上传组件

---
+++ demo/pages/FormRenderMini/FormImageUpload/index.ts
@@ -0,0 +1,133 @@
+const onUpload = (file) => {
+  console.log('<<<<file', file);
+  return file.path;
+};
+Page({
+  data: {
+    schema: {
+      formId: 'demo-image-upload',
+      type: 'object',
+      radius: false,
+      displayType: 'row',
+      operation: {
+        flex: true,
+        fixedFooter: true,
+        footer: [
+          {
+            text: '提交',
+            type: 'primary',
+          },
+        ],
+      },
+      properties: {
+        card: {
+          title: '内置组件 image-upload 的使用',
+          type: 'object',
+          widget: 'card',
+          properties: {
+            basicImageUpload: {
+              title: '基础图片上传',
+              type: 'array',
+              widget: 'image-upload',
+              props: {
+                maxCount: 3,
+                onUpload: (file) => onUpload(file),
+                multiple: true,
+                uploadText: '上传图片'
+              }
+            },
+            singleImageUpload: {
+              title: '单图上传',
+              type: 'string',
+              widget: 'image-upload',
+              props: {
+                maxCount: 1,
+                multiple: false,
+                uploadText: '上传单张图片'
+              }
+            },
+            defaultImageUpload: {
+              title: '默认图片',
+              type: 'array',
+              widget: 'image-upload',
+              value: [
+                {
+                  url: 'https://gw.alipayobjects.com/mdn/rms_226d75/afts/img/A*znK_ToIL8rQAAAAAAAAAAAAAARQnAQ',
+                }
+              ],
+              props: {
+                maxCount: 2,
+                multiple: true,
+                uploadText: '上传图片'
+              }
+            },
+            disabledImageUpload: {
+              title: '禁用状态',
+              type: 'array',
+              widget: 'image-upload',
+              value: [
+                {
+                  url: 'https://gw.alipayobjects.com/mdn/rms_226d75/afts/img/A*znK_ToIL8rQAAAAAAAAAAAAAARQnAQ',
+                }
+              ],
+              props: {
+                maxCount: 3,
+                multiple: true,
+                uploadText: '上传图片',
+                disabled: true
+              }
+            },
+            customImageUpload: {
+              title: '自定义样式',
+              type: 'array',
+              widget: 'image-upload',
+              props: {
+                maxCount: 3,
+                multiple: true,
+                uploadText: '上传图片',
+                className: 'custom-image-upload'
+              }
+            },
+            limitImageUpload: {
+              title: '限制上传数量',
+              type: 'array',
+              widget: 'image-upload',
+              props: {
+                maxCount: 3,
+                multiple: true,
+                uploadText: '最多上传3张图片'
+              }
+            },
+            previewImageUpload: {
+              title: '预览模式',
+              type: 'array',
+              widget: 'image-upload',
+              value: [
+                {
+                  url: 'https://gw.alipayobjects.com/mdn/rms_226d75/afts/img/A*znK_ToIL8rQAAAAAAAAAAAAAARQnAQ',
+                },
+                {
+                  url: 'https://gw.alipayobjects.com/mdn/rms_226d75/afts/img/A*kStORbDQxwMAAAAAAAAAAAAAARQnAQ',
+                }
+              ],
+              props: {
+                maxCount: 2,
+                multiple: true,
+                uploadText: '上传图片',
+                onPreview: (file) => {
+                  my.previewImage({
+                    urls: [file.url]
+                  });
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  
+  onTapOperation(e) {
+    console.log('操作按钮点击', e);
+  }
+}); 
---
+++ src/SolutionForm/basic-component/image-upload/index.ts
@@ -0,0 +1,45 @@
+import { createComponent } from '@alipay/merchant-base-mini';
+import { getStore } from '../../store/index';
+
+createComponent({
+  onInit() {
+    this.store = getStore();
+  },
+  methods: {
+    onChange(v) {
+      this.props?.onTriggerChange(v, this.props.field);
+    },
+    onBeforeUpload(localFileList) {
+      if (this.props.config?.onBeforeUpload) {
+        return this.props.config.onBeforeUploadlocalFileList();
+      }
+      return localFileList;
+    },
+    onUpload(file) {
+      return new Promise(async (resolve, reject) => {
+        if (this.props.config?.onUpload) {
+          const res = await this.props.config.onUpload(file);
+          resolve(res);
+        } else {
+          reject('图片上传的 onUpload 方法是必传参数');
+        }
+      });
+    },
+    onRemove(file) {
+      if (this.props.config?.onRemove) {
+        return this.props.config.onRemove(file);
+      }
+      return true;
+    },
+    onChooseImageError(error) {
+      if (this.props.config?.onChooseImageError) {
+        return this.props.config.onChooseImageError(error);
+      }
+    },
+    onPreview(file) {
+      if (this.props.config?.onPreview) {
+        return this.props.config.onPreview(file);
+      }
+    },
+  },
+});
