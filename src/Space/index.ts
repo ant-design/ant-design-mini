907e02e68a92fc5013804e85c39ce842bb46ed00
rayhomie <1572801584@qq.com>
2025-07-30 12:57:08 +0800

    feat: 不支持alipaynative环境的提示

--- src/Space/index.ts
+++ src/Space/index.ts
@@ -1,4 +1,7 @@
 import { Component } from '../_util/simply';
+import { assertAilpayNativeNotSupport } from '../_util/support';
 import { SpaceDefaultProps } from './props';
+
+assertAilpayNativeNotSupport('Space');
 
 Component({ props: SpaceDefaultProps });
